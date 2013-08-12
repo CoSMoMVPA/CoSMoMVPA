d990e426982bc0ddf44eb474ff4f884f9e2af761
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2013-08-12 15:46:03 +0200

    ENH: minor

--- scripts/src/exercise_dataset_basics.m
+++ scripts/src/exercise_dataset_basics.m
@@ -1,15 +1,25 @@
 %% Dataset basics
-% Load the dataset with VT mask
+% Set data path, load dataset, set targets and chunks, and add labels as
+% sample attributes
+
+% Set the data path (change cosmo_get_data_path if necessary)
 data_path=cosmo_get_data_path('s01');
 
+% Load dataset
+% >>
 ds = fmri_dataset([data_path '/glm_T_stats_perrun.nii.gz'], ...
                     'mask', [data_path '/vt_mask.nii.gz']);
+% <<
 
-% set the targets and the chunks
+% Set the targets and the chunks
+% >>
 ds.targets = repmat([1:6],1,10);
 chunks = []; for i=1:10 chunks = [chunks repmat(i,1,6)]; end
 ds.chunks = chunks;
+% <<
 
 % Add labels as sample attributes
+% >>
 labels = {'monkey','lemur','mallard','warbler','ladybug','lunamoth'};
 ds.sa.labels = repmat(labels,1,10)
+% <<
--- scripts/src/exercise_splithalf_correlations.m
+++ scripts/src/exercise_splithalf_correlations.m
@@ -1,16 +1,23 @@
-% roi-based MVPA with group-analysis
+%% roi-based MVPA with group-analysis
+%
+% Load t-stat data from all subjects, apply 'ev' mask, compute difference
+% of (fisher-transformed) between on- and off diagonal split-half
+% correlation values, and perform a random effects analysis
 
 subject_ids={'s01','s02','s03','s04'};
-roi='brain'; % 'vt' or 'ev' or 'brain'
+roi='ev'; % 'vt' or 'ev' or 'brain'
 data_type='T_stats'; % 'T_stats' or 'betas'
 
 nsubjects=numel(subject_ids);
 
+% allocate space for output
 mean_weighted_zs=zeros(nsubjects,1);
+
+% do compututation for each subject 
 for j=1:nsubjects
     subject_id=subject_ids{j};
 
-    datadir=sprintf('../data/small/%s/stats/',subject_id) ;
+    datadir=cosmo_get_data_path(subject_id);
     fn1=sprintf('%s/glm_%s_evens.nii.gz', datadir, data_type);
     fn2=sprintf('%s/glm_%s_odds.nii.gz', datadir, data_type);
     fn_mask=sprintf('%s/%s_mask.nii.gz', datadir, roi);
@@ -23,7 +30,6 @@ for j=1:nsubjects
     half2_samples_4D=ni_samples2.img;
 
     % define the feature (voxel) mask
-    %
     feature_mask=logical(ni_mask.img);
     nfeatures=sum(feature_mask(:));
     
@@ -35,9 +41,10 @@ for j=1:nsubjects
     % allocate space for data of the two halves in the ROI
     half1_samples_masked=zeros(nsamples_per_half, nfeatures);
     half2_samples_masked=zeros(nsamples_per_half, nfeatures);
-        
-    % >> store the values in the mask [ use a 'for' loop to
-    % >> go from 1 to nsamples_per_half, and use 'feature_mask' ]
+
+    % store the values in the mask (use a 'for' loop to
+    % go from 1 to nsamples_per_half, and use 'feature_mask')
+    % >> 
     for k=1:nsamples_per_half
         half1_sample_3D=half1_samples_4D(:,:,:,k);
         half1_samples_masked(k,:)=half1_sample_3D(feature_mask);
@@ -48,9 +55,10 @@ for j=1:nsubjects
     end
     % <<
     
-    % >> compute the average of the fisher-transformed correlation
-    % >> values between the two halves, and store this result
-    % >> in 'mean_weighted_z' [Use 'atanh' and '.*'] 
+    % compute the average of the fisher-transformed correlation
+    % values between the two halves, and store this result
+    % in 'mean_weighted_z' [Use 'atanh' and '.*'].
+    % >>
     rho=corr(half1_samples_masked',half2_samples_masked');
     z=atanh(rho);
 
@@ -61,7 +69,6 @@ for j=1:nsubjects
     weighted_z=z .* contrast_matrix;
 
     mean_weighted_z=mean(weighted_z(:));
-    
     % <<
     
     % store the result for this subject
