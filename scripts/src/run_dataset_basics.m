64f54ab8708d9a02de922c5e9b4011f6034b9229
andy <andycconnolly@gmail.com>
2013-08-13 10:51:38 -0400

    fixed typo in dataset basics

--- scripts/src/run_dataset_basics.m
+++ scripts/src/run_dataset_basics.m
@@ -13,12 +13,12 @@ ds = cosmo_fmri_dataset([data_path '/glm_T_stats_perrun.nii.gz'], ...
 
 % Set the targets and the chunks
 %
-% There are 10 runs with 6 volumes per run. The runs are vertically stacked on
-% above the other. The six volume in each run correspond to the stimuli:
-% 'monkey','lemur','mallard','warbler','ladybug','lunamoth'. Add numeric samples
-% such that 1 corresponds to 'monkey', 2 corresponds to 'lemur', etc. Then add
-% numeric chunk attributes so that 1 corresponds to run1, 2 corresponds to run2,
-% etc.
+% There are 10 runs with 6 volumes per run. The runs are vertically stacked one
+% above the other. The six volumes in each run correspond to the stimuli:
+% 'monkey','lemur','mallard','warbler','ladybug','lunamoth', in that order. Add
+% numeric targets labels (samples atribute) such that 1 corresponds to 'monkey',
+% 2 corresponds to 'lemur', etc. Then add numeric chunks (another samples
+% attribute) so that 1 corresponds to run1, 2 corresponds to run2, etc.
 
 %>>
 ds.sa.targets = repmat([1:6]',10,1); chunks = []; for i=1:10 chunks = [chunks;
