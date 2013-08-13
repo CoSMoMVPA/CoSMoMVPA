3a88369e26f41ded8b4d8fda916d3ca5f73ebdd1
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2013-08-13 12:41:06 +0200

    RF: completed measure renaming

--- scripts/src/cosmo_cross_validation_measure.m
+++ scripts/src/cosmo_cross_validation_measure.m
@@ -1,4 +1,4 @@
-function accuracy = cosmo_cross_validate(dataset, args)
+function accuracy = cosmo_cross_validation_measure(dataset, args)
 % performs cross-validation using a classifier
 %
 % accuracy = cosmo_cross_validate(dataset, args)
@@ -12,40 +12,13 @@ function accuracy = cosmo_cross_validate(dataset, args)
 %                       @classify_naive_baysian
 %   args.partitions          For example the output from nfold_partition
 %   
-% NNO Aug 2013, 
-% modified by ACC. Modified to conform to signature of generic datset 'measure'
+% ACC. Modified to conform to signature of generic datset 'measure'
+% NNO made this a wrapper function
 
 if ~isfield(args,'opt') args.opt = struct(); end
 if ~isfield(args,'classifier') error('Missing input args.classifier'); end
 if ~isfield(args,'partitions') error('Missing input args.partitions'); end
 
-
-train_indices = args.p
-artitions.train_indices;
-test_indices = args.partitions.test_indices;
-
-npartitions=numel(train_indices);
-
-[nsamples,nfeatures]=size(dataset.samples);
-
-pred=zeros(nsamples,1); % space for output
-ncorrect=0; % how many samples were correctly classified
-ntotal=0; % how many samples were classified (correctly or not)
-
 % >>
-for k=1:npartitions
-    train_data = dataset.samples(train_indices{k},:);
-    test_data = dataset.samples(test_indices{k},:);
-    
-    train_targets = dataset.sa.targets(train_indices{k});
-    
-    p = args.classifier(train_data, train_targets, test_data, opt);
-    pred(test_indices{k}) = p;
-    
-    test_targets = dataset.sa.targets(test_indices{k});
-    ncorrect = ncorrect + sum(p(:) == test_targets(:));
-    ntotal = ntotal + numel(test_targets);
-end
+accuracy=cosmo_cross_validate(dataset, args.classifier, args.partitons, args.opt);
 % <<
-
-accuracy = ncorrect/ntotal;
