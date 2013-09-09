986e21cbafb25bd0a300da229c12f38cfc726154
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2013-09-09 19:35:14 -0400

    NF: allow cosmo_corr to use corr for other than Pearson correlations

--- mvpa/cosmo_corr.m
+++ mvpa/cosmo_corr.m
@@ -1,32 +1,67 @@
-function c=cosmo_corr(x,y)
-% Computes pearson correlation - faster than than matlab's "corr".
+function c=cosmo_corr(x,y,corr_type)
+% Computes correlation - faster than than matlab's "corr" for Pearson.
 %
-% c=comso_corr(x[,y])
+% c=comso_corr(x[,y[,corr_type]])
 %
 % Inputs:
-%   x      PxM matrix
-%   y      PxN matrix (optional). If omitted then y=x.
+%   x          PxM matrix.
+%   y          PxN matrix (optional). If omitted then y=x.
+%   corr_type  'Pearson' or 'Spearman' or 'Kendall' (optional). If omitted 
+%              then corrtype='Pearson' and the computation time is
+%              significantly reduced for small matrices x and y (with 
+%              /tiny/ numerical imprecisions).
 % Output:
-%   c      MxN matrix with c(i,j)=corr(x(:,i),y(:,j))
+%   c          MxN matrix with c(i,j)=corr(x(:,i),y(:,j),'type',corr_type).
+%
+% Notes:
+%  - this function does not compute probability values.
+%
+% Example:
+% - % generate some random data. 
+%   clear all
+%   x=randn(100); y=randn(100);
+%   % call the function first to avoid lookup delays; then measure time
+%   corr(x,y); corr(x,y); tic; c=corr(x,y); toc
+%   cosmo_corr(x,y); cosmo_corr(x,y); tic; cc=cosmo_corr(x,y); toc
+%   % compute differences in output
+%   delta=c-cc;
+%   max_delta=max(abs(delta(:)));
+%   n_eps=eps/max_delta; % how many epsilons
+%   fprintf('max difference: %d (%d epsilons)\n',max_delta,n_eps)
+%   % output:
+%   > Elapsed time is 0.014310 seconds.
+%   > Elapsed time is 0.001418 seconds.
+%   > max difference: 1.110223e-16
 %
 % See also: corr
 %
-% NNO Sep 2013 (from phoebe_corr, July 2010)
+% NNO Sep 2013 (from NNO's phoebe_corr, July 2010)
 
-if nargin<2
+if nargin<2 || isempty(y)
     y=x;
 end
 
-% subtract mean
-xd=bsxfun(@minus,x,mean(x));
-yd=bsxfun(@minus,y,mean(y));
+if nargin<3 || isempty(corr_type)
+    corr_type='Pearson';
+end
 
-% normalization
-n=1/(size(x,1)-1);
+switch corr_type
+    case 'Pearson'
+        % subtract mean
+        xd=bsxfun(@minus,x,mean(x));
+        yd=bsxfun(@minus,y,mean(y));
 
-% standard deviation
-xs=bsxfun(@power,n*sum(xd .* xd),-0.5);
-ys=bsxfun(@power,n*sum(yd .* yd),-0.5);
+        % normalization
+        n=1/(size(x,1)-1);
 
-% compute correlations
-c=n * (xd' * yd) .* (xs' * ys);
+        % standard deviation
+        xs=(n*sum(xd .^ 2)).^-0.5;
+        ys=(n*sum(yd .^ 2)).^-0.5;
+
+        % compute correlations
+        c=n * (xd' * yd) .* (xs' * ys);
+    otherwise
+        'fallback'
+        % fall-back: use Matlab's function
+        c=corr(x,y,'type',corr_type);
+end
