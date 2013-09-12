b4bb822f67603e4e6257e3c82e57f2da3b2e9be5
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2013-09-11 21:36:15 -0400

    ENH: more difficult dataset for testing

--- tests/generate_test_dataset.m
+++ tests/generate_test_dataset.m
@@ -2,4 +2,5 @@ function ds=generate_test_dataset()
 
     ni=generate_test_nifti_struct();
     ds=cosmo_fmri_dataset(ni,'chunks',floor(((1:20)-1)/4)+1,...
-                             'targets',repmat([1:5]',4,1));
+                             'targets',repmat([1:4]',5,1));
+    ds.sa.labels=repmat({'a','bb','c','d','e'}',4,2);
