126e1f8d846f684c58189dec5f407c2650191efe
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2013-09-15 22:07:51 -0400

    NF+LIC: LDA classifier. Derived from "classify_lda_KclassesQuicker" by Joern Diedrichsen, Tobias Wiestler, NNO, August 2008"

--- doc/source/thanks.rst
+++ doc/source/thanks.rst
@@ -4,8 +4,8 @@ Developers
 ----------
 CoSMoMVPA is developed by:
 
-- [Nikolaas N. Oosterhof](http://haxbylab.dartmouth.edu/ppl/nno.html).
-- [Andrew C. Connolly](http://haxbylab.dartmouth.edu/ppl/andy.html).
+- `Nikolaas N. Oosterhof <http://haxbylab.dartmouth.edu/ppl/nno.html>`_
+- `Andrew C. Connolly <http://haxbylab.dartmouth.edu/ppl/andy.html>`_
 
 Acknowledgements
 ----------------
@@ -16,3 +16,6 @@ We would like to thank the following people:
 +  **Michael Hanke** and **Yaroslav Halchenko**: for their work on PyMVPA, which inspired the semantics and data structure of datasets in CoSMoMVPA.
 +  **Stephania Bracci**: for providing us with sample datasets in BrainVoyager.
 +  **Jimmy Shen**: for providing a free/open source NIFTI library. 
++  **Joern Diedrichsen** and **Tobias Wiestler** for contributions in the LDA classifier.
+
+
---
+++ mvpa/cosmo_classify_lda.m
@@ -0,0 +1,70 @@
+function predicted = cosmo_classify_lda(samples_train, targets_train, samples_test, opt)
+% naive bayes classifier
+%
+% predicted=cosmo_classify_lda(samples_train, targets_train, samples_test[,opt])
+%
+% Inputs
+% - samples_train      PxR training data for P samples and R features
+% - targets_train      Px1 training data classes
+% - samples_test       QxR test data
+%-  opt                Optional struct with optional field:
+%   regularization     Used to regularize covariance matrix. Default .01
+%
+% Output
+% - predicted          Qx1 predicted data classes for samples_test
+%
+% Joern Diedrichsen, Tobias Wiestler, NNO Nov 2008; NNO updated Aug 2013
+
+if nargin<4 || isempty(opt) || ~isfield(opt, 'regularization')
+    opt.regularization=.01;
+end
+
+[ntrain, nfeatures]=size(samples_train);
+[ntest, nfeatures_]=size(samples_test);
+ntrain_=numel(targets_train);
+
+if nfeatures~=nfeatures_ || ntrain_~=ntrain, error('illegal input size'); end
+
+classes=unique(targets_train);
+nclasses=numel(classes);    
+
+class_mean=zeros(nclasses,nfeatures);   % class means
+class_cov=zeros(nfeatures);              % within-class variability 
+
+% compute mean and (co)variance
+for k=1:nclasses;
+    % select data in this class
+    msk=targets_train==classes(k);  
+    
+    % number of samples in k-th class
+    n=sum(msk); 
+    
+    if k==1
+        if n<2
+            error('Need at least two samples per class in training');
+        end
+        nfirst=n; % keep track of number of samples
+    elseif nfirst~=n
+        error(['Different number of classes (%d and %d) - this is '...
+                'not supported'], n, nfirst);
+    end
+    
+    class_samples=samples_train(msk,:);
+    
+    class_mean(k,:) = sum(class_samples,1)/n; % class mean
+    res = bsxfun(@minus,class_samples,class_mean(k,:)); % residuals
+    class_cov = class_cov+res'*res; % estimate common covariance matrix
+end;
+% apply regularization
+regularization=opt.regularization;
+class_cov=class_cov/ntrain; 
+reg=eye(nfeatures)*trace(class_cov)/nfeatures;
+class_cov_reg=class_cov+reg*regularization;
+
+% linear discriminant
+class_weight=class_mean/class_cov_reg; 
+class_offset=sum(class_weight .* class_mean,2);
+class_proj=bsxfun(@plus,-.5*class_offset,class_weight*samples_test');
+
+[foo,class_idxs]=max(class_proj);
+predicted=classes(class_idxs);
