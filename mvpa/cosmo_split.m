5bcbc3482fec090756c422bf31ceeee65c5cc3b5
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2013-09-16 16:58:17 -0400

    NF: cosmo_split to split based on feature or sample attributes

---
+++ mvpa/cosmo_split.m
@@ -0,0 +1,85 @@
+function ds_splits=cosmo_split(ds, split_by, dim)
+% splits a dataset by unique values in a sample or feature attribute
+%
+% cosmo_split(ds, split_by, dim)
+%
+% Inputs:
+%   ds          dataset struct
+%   split_by    fieldname for the sample (if dim==1) or feature (if dim==2)
+%               attribute by which the dataset is split.
+%               It can also be a cell with fieldnames, in which case the
+%               dataset is split by the set of combinations from these
+%               fieldnames.
+%   dim         1 (split by samples; default) or 2 (split by features).
+%
+% Returns:
+%   ds_splits   1xP cell, if there are P unique values for the attribute
+%               indicated by split_by and dim.
+%
+% NNO Sep 2013
+
+% set default dim
+if nargin<3 || isempty(dim), dim=1; end
+
+cosmo_check_dataset(ds);
+
+if iscell(split_by)
+    ndim=numel(split_by);
+
+    if ndim>1
+        % more than one fieldname to split; use recursion
+        
+        me=str2func(mfilename()); % make imune to renaming
+        
+        % split by first fieldname
+        split_head=me(ds, split_by{1}, dim);
+        nhead=numel(split_head);
+        
+        % allocate space for remaining splits
+        split_all=cell(1,nhead);
+        
+        % remaining fieldnames 
+        split_by_tail={split_by{2:end}};
+        
+        % split each of them
+        for k=1:nhead
+            split_all{k}=me(split_head{k}, split_by_tail, dim);
+        end
+        
+        % join results
+        ds_splits=[split_all{:}];
+        return
+    end
+   
+    % just one field, get it out and continue
+    split_by=split_by{1};
+end
+
+if all(dim~=[1 2]), error('dim should be 1 or 2'); end
+
+attrs_fns={'sa','fa'};
+attrs_fn=attrs_fns{dim};
+
+% ensure the field is there
+if ~isfield(ds, attrs_fn) || ~isfield(ds.(attrs_fn), split_by)
+    error('missing field .%s.%s', attrs_fn, split_by); 
+end
+
+% attribute values
+values=ds.(attrs_fn).(split_by);
+if ~isnumeric(values),
+    error('field .%s.%s must be numeric', attrs_fn, split_by);
+end
+split_values=unique(values);
+nsplits=numel(split_values);
+
+ds_splits=cell(1,nsplits);
+for k=1:nsplits
+    ds_splits{k}=cosmo_slice(ds, values==split_values(k));
+end
+
+
+
+
+
+
