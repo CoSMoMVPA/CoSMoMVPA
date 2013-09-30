40da32bc0b2e17d222cfb5f6464f4fff73db3cba
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2013-09-29 21:46:14 -0400

    NF: general matching function for numerial arrays and strings

---
+++ mvpa/cosmo_match.m
@@ -0,0 +1,135 @@
+function msk=cosmo_match(varargin)
+% returns a mask indicating matching occurences in two arrays or cells
+% relative to the second array
+%
+% msk=cosmo_match(needle1, haystack1, [needle2, haystack2, ...)
+%
+% Inputs:
+%   needle*           numeric vector, or cell with strings. A string is
+%                     also allowed and interpreted as {needle}.
+%   haystack*         numeric vector, or cell with strings. A string is
+%                     also allowed and interpreted as the name of a feature
+%                     dimension ('i','j' or 'k' in fmri datasets; 'chan',
+%                     'time', or 'freq' in MEEG datasets), and its 
+%                     respective values (from ds.a.dim.values{dim}, where
+%                     dim is the dimension corresponding to haystack) as
+%                     indexed by ds.fa.(haystack) are used as haystack.
+%   
+% Output:
+%   msk               boolean array of the same size as haystack, with
+%                     true where the value in haystack is equal to at least
+%                     one value in needle. If multiple needle/haystack
+%                     pairs are provided, then the haystack inputs should 
+%                     have the same number of elements, and msk contains 
+%                     the intersection of the individual masks. 
+%                     
+% Examples (in all cases, ds is assumed to be a dataset struct):
+%   - % in an fMRI dataset, get mask for features with with first spatial
+%     % dimension indices 5 or 7.
+%     msk=cosmo_match(ds.fa.i,[5 7]);
+%
+%   - % get mask for chunk values 1 and 4
+%     msk=cosmo_match(ds.sa.chunks,[1 4]);
+%
+%   - % get mask for chunk values 1 and 4, and target values 1, 3 or 6.
+%     msk=cosmo_match(ds.sa.chunks, [1 4], ds.sa.targets, [1 3 6]);
+%
+%   - % get feature mask for the fourth channel in an MEEG dataset
+%     msk=cosmo_match(ds.fa.chan,4)
+%
+% Note:
+%   - the output of this function can be used with cosmo_slice
+%     to select features or samples of interest
+%
+% See also: cosmo_slice, cosmo_stack
+%
+% NNO Sep 2013
+
+
+if nargin<2
+    error('Need at least two input arguments')
+elseif mod(nargin,2) ~= 0
+    error('Need an even number of input arguments');
+end
+
+haystack=varargin{1};
+needle=just_convert_str2cell(varargin{2});
+
+if isnumeric(haystack) && isnumeric(needle) && numel(needle)==1
+    % optimization for most standard case: vector and scalar
+    check_vec(haystack);
+    msk=needle==haystack;
+else
+    tp=get_type( needle, haystack);
+    nrows=check_vec(needle);
+    ncols=check_vec(haystack);
+
+    switch tp
+        case 'numeric'
+            matches=bsxfun(@eq, needle(:), haystack(:)');
+
+        case 'cell_with_strings'
+            matches=false(nrows,ncols);
+            for k=1:nrows
+                match_indices=strmatch(needle{k}, haystack, 'exact');
+                matches(k,match_indices)=true;
+            end
+    end
+    msk=reshape(sum(matches,1)>0,size(haystack));
+end
+
+if nargin>3
+    me=str2func(mfilename()); % immune to renaming
+    other_msk=me(varargin{3:end});  % use recursion
+    
+    % check the size is the same
+    if numel(msk) ~= numel(other_msk)
+        error('Cannot make conjunction: masks have %d ~= %d elements',...
+                numel(msk), numel(other_msk));
+    end
+    
+    % conjunction
+    msk=msk & other_msk;
+end
+
+
+function c=just_convert_str2cell(x)
+% only if x is a string, it is converted to char. otherwise x is returned
+if ischar(x)
+    c={x};
+else
+    c=x;
+end
+
+function n=check_vec(x)
+% checks whether it's a vector, and if so, returns the number of elements
+sz=size(x);
+if numel(sz) ~= 2 || sum(sz>1)>1
+    error('Input argument is not a vector');
+end
+n=numel(x);
+        
+function tp=get_type(needle, haystack)
+% returns a string indicating the type of needle and haystack. If needle
+% and haystack have different types an error is thrown
+
+types=struct();
+types.numeric=@isnumeric;
+types.cell_with_strings=@(x) iscell(x) && all(cellfun(@ischar,x));
+
+fns=fieldnames(types);
+for k=1:numel(fns)
+    fn=fns{k};
+    check_func=types.(fn);
+    
+    if check_func(needle) 
+        if check_func(haystack)
+            tp=fn; % matching type found
+            return
+        else
+            error('first argument type %s but second is not',fn);
+        end
+    end
+end
+
+error('Unsupported type; type not one of %s', sprintf('%s ', fns{:}));
