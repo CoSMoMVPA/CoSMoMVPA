fca5b24acb745829a2e28620cae0406bbb0eeacf
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2013-10-13 17:10:03 -0400

    DOC+RF+BF: use "ds" as variable, and deal properly with samples that have no prediction

--- mvpa/cosmo_cross_validate.m
+++ mvpa/cosmo_cross_validate.m
@@ -1,10 +1,10 @@
-function [pred, accuracy] = cosmo_cross_validate(dataset, classifier, partitions, opt)
+function [pred, accuracy] = cosmo_cross_validate(ds, classifier, partitions, opt)
 % performs cross-validation using a classifier
 %
 % [pred, accuracy] = cosmo_cross_validate(dataset, classifier, partitions, opt)
 % 
 % Inputs
-%   dataset             struct with fields .samples (PxQ for P samples and 
+%   ds                  struct with fields .samples (PxQ for P samples and 
 %                       Q features) and .sa.targets (Px1 labels of samples)
 %   classifier          function handle to classifier, e.g.
 %                       @classify_naive_baysian
@@ -20,21 +20,28 @@ function [pred, accuracy] = cosmo_cross_validate(dataset, classifier, partitions
         opt=struct();
     end
     
+    % optionally de-mean or zscore the data
+    % since class label information is not used here, there is no circular
+    % analysis problem.
+    if isfield(opt,'normalize')
+        ds=cosmo_normalize(ds, opt.normalize);
+    end
+    
     train_indices = partitions.train_indices;
     test_indices = partitions.test_indices;
     
     npartitions=numel(train_indices);
     
-    [nsamples,nfeatures]=size(dataset.samples);
+    [nsamples,nfeatures]=size(ds.samples);
     
     all_pred=zeros(nsamples,npartitions); % space for output (one column per partition)
     test_mask=false(nsamples,1); % indicates for which samples there has been a prediction
     for k=1:npartitions
         % >>
-        train_data = dataset.samples(train_indices{k},:);
-        test_data = dataset.samples(test_indices{k},:);
+        train_data = ds.samples(train_indices{k},:);
+        test_data = ds.samples(test_indices{k},:);
         
-        train_targets = dataset.sa.targets(train_indices{k});
+        train_targets = ds.sa.targets(train_indices{k});
         
         p = classifier(train_data, train_targets, test_data, opt);
         
@@ -44,7 +51,7 @@ function [pred, accuracy] = cosmo_cross_validate(dataset, classifier, partitions
     end
     
     [pred,classes]=cosmo_winner_indices(all_pred);
-    correct_mask=dataset.sa.targets(test_mask)==classes(pred);
+    correct_mask=ds.sa.targets(test_mask)==classes(pred(pred>0));
     ncorrect = sum(correct_mask);
     ntotal = numel(correct_mask);
     
