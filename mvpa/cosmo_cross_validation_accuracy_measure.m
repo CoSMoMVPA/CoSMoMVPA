3888be9286960573647675f9787328519659d4fc
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2013-10-13 17:10:40 -0400

    RF: use args for cross-val measure directly in classifier as well

--- mvpa/cosmo_cross_validation_accuracy_measure.m
+++ mvpa/cosmo_cross_validation_accuracy_measure.m
@@ -6,11 +6,14 @@ function ds_sa = cosmo_cross_validation_accuracy_measure(dataset, args)
 % Inputs
 %   dataset             struct with fields .samples (PxQ for P samples and 
 %                       Q features) and .sa.targets (Px1 labels of samples)
-%   args                struct containing classifier, partitions, and opt (which
-%                           is optional)
+%   args                struct containing classifier, partitions, and
+%                       possibly other fields that are given to the
+%                       classifier.
 %   args.classifier     function handle to classifier, e.g.
 %                       @classify_naive_baysian
-%   args.partitions          For example the output from nfold_partition
+%   args.partitions     Partition scheme, for example the output from 
+%                       cosmo_nfold_partition
+%   
 %
 % Output
 %    ds_sa        Struct with fields:
@@ -21,14 +24,18 @@ function ds_sa = cosmo_cross_validation_accuracy_measure(dataset, args)
 % ACC. Modified to conform to signature of generic datset 'measure'
 % NNO Aug 2013 made this a wrapper function
     
-if ~isfield(args,'opt') args.opt = struct(); end
 if ~isfield(args,'classifier') error('Missing input args.classifier'); end
 if ~isfield(args,'partitions') error('Missing input args.partitions'); end
 
 % Run cross validation to get the accuracy (see the help of
 % cosmo_cross_validate)
 % >>
-[pred, accuracy]=cosmo_cross_validate(dataset, args.classifier, args.partitions, args.opt);
+classifier=args.classifier;
+partitions=args.partitions;
+
+args=rmfield(args,'classifier');
+args=rmfield(args,'partitions');
+[pred, accuracy]=cosmo_cross_validate(dataset, classifier, partitions, args);
 % <<
 
 ds_sa=struct();
