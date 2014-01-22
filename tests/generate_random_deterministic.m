a6e2e57e68b162d235ad8d726e5f81d1728db7bc
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-01-22 16:15:08 +0100

    RF+NF: rename generate_randn_deterministic -> generate_random_deterministic and support other distributions than normal

---
+++ tests/generate_random_deterministic.m
@@ -0,0 +1,14 @@
+function data=generate_random_deterministic(shape, fhandle)
+% generate deterministic data. default with gaussian distribution
+if nargin<2, fhandle=@randn; end
+
+rng_state=rng; % store state
+
+try
+    rng('default') % get default state
+    data=fhandle(shape); % generate random data
+catch
+end
+
+% ensure that state is restored even if randn raised an exception
+rng(rng_state); 
