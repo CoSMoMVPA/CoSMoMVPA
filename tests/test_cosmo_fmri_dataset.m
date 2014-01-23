c4865a5f0df5cda860e1944e5f7317339d900009
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-01-23 20:27:42 +0100

    ENH+TST: test for chunks and targets argument

--- tests/test_cosmo_fmri_dataset.m
+++ tests/test_cosmo_fmri_dataset.m
@@ -10,16 +10,21 @@ function test_init()
 
 function test_nifti()
     ni=generate_test_nifti_struct();
+    g=generate_test_dataset();
+    
     tmpfn='__tmp_.nii';
     save_nii(ni,tmpfn);
-    ds=cosmo_fmri_dataset(tmpfn);
+    ds=cosmo_fmri_dataset(tmpfn,'targets',g.sa.targets,...
+                                'chunks',g.sa.chunks);
     delete(tmpfn)
 
-    g=generate_test_dataset();
+    
     assertTrue(numel(setxor(fieldnames(ds),...
                fieldnames(g)))==0);
 
     assertElementsAlmostEqual(ds.samples, g.samples);
+    assertEqual(ds.sa.targets, g.sa.targets)
+    assertEqual(ds.sa.chunks, g.sa.chunks)
 
 function test_io()
     ds=generate_test_dataset();
