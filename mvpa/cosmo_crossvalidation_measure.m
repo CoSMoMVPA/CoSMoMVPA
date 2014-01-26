c0245ae7b4d95a0a14287a769ccdc295a5bb142b
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-01-26 16:47:46 +0100

    NF: supoprt returning predictions

--- mvpa/cosmo_crossvalidation_measure.m
+++ mvpa/cosmo_crossvalidation_measure.m
@@ -1,4 +1,4 @@
-function ds_sa = cosmo_cross_validation_accuracy_measure(dataset, args)
+function ds_sa = cosmo_crossvalidation_measure(dataset, varargin)
 % performs cross-validation using a classifier
 %
 % accuracy = cosmo_cross_validation_accuracy_measure(dataset, args)
@@ -13,32 +13,47 @@ function ds_sa = cosmo_cross_validation_accuracy_measure(dataset, args)
 %                       @classify_naive_baysian
 %   args.partitions     Partition scheme, for example the output from 
 %                       cosmo_nfold_partition
-%   
+%   args.output         'accuracy' (default) or 'predictions'
 %
 % Output
 %    ds_sa        Struct with fields:
 %      .samples   Scalar with classification accuracy.
 %      .sa        Struct with field:
-%        .labels  {'acc'}
+%                 - if args.output=='accuracy':
+%                       .labels  =={'accuracy'}
+%                 - if args.output=='predictions'
+%                       .targets     } Px1 real and predicted labels of 
+%                       .predictions } each sample
+%        
 %   
 % ACC. Modified to conform to signature of generic datset 'measure'
 % NNO Aug 2013 made this a wrapper function
-    
-if ~isfield(args,'classifier') error('Missing input args.classifier'); end
-if ~isfield(args,'partitions') error('Missing input args.partitions'); end
+
+% deal with input arguments
+params=cosmo_structjoin('output','accuracy',... % default output
+                        varargin); % use input arguments
 
 % Run cross validation to get the accuracy (see the help of
 % cosmo_cross_validate)
 % >@@>
-classifier=args.classifier;
-partitions=args.partitions;
+classifier=params.classifier;
+partitions=params.partitions;
 
-args=rmfield(args,'classifier');
-args=rmfield(args,'partitions');
-[pred, accuracy]=cosmo_cross_validate(dataset, classifier, partitions, args);
+params=rmfield(params,'classifier');
+params=rmfield(params,'partitions');
+[pred, accuracy]=cosmo_cross_validate(dataset, classifier, partitions, params);
 % <@@<
 
 ds_sa=struct();
-ds_sa.samples=accuracy;
-ds_sa.sa.labels={'acc'};
+
+switch params.output
+    case 'accuracy'
+        ds_sa.samples=accuracy;
+        ds_sa.sa.labels={'accuracy'};
+    case 'predictions'
+        ds_sa.sa.targets=dataset.sa.targets;
+        ds_sa.sa.predictions=pred(:);
+    otherwise
+        error('Illegal output parameter %s', params.output);
+end
     
