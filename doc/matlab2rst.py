0f46bf79d79e04e353b8d3df3d50f609fcff3426
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-01-28 18:54:42 +0100

    RF+ENH: simplify matlab2rst conversion

--- doc/matlab2rst.py
+++ doc/matlab2rst.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # converts matlab to rst files
 #
-# This code reads all matlab .m files and then generates three versions:
-# 1) the full file, but with lines '% >>' and '<<' removed
+# This code reads all matlab .m files and then generates multiple versions:
+# 
+# 1) the full file, but with lines '% >@@>' and '<<' removed
 # 2) the file with code in between '% >>' and '<<' replaced by
 #    a comment saying 'your code here' (postfix '_skl' for skeleton)
 # 3) the file with only the header (first line plus all lines up to
@@ -20,11 +21,15 @@ import glob
 from os.path import join, split, getmtime, isfile
 matlab_dir='../mvpa'
 example_dir='../examples'
-trg_dir='source'
-rst_sub_dir='matlab'
-modindex_dir=trg_dir
 
-for d in (trg_dir, join(trg_dir, rst_sub_dir), modindex_dir):
+output_root_abs='source'
+output_index_abs=output_root_abs
+output_mat_rel='matlab'
+output_mat_abs=join(output_root_abs, output_mat_rel)
+
+publish_rel=join('_static/publish')
+
+for d in (output_root_abs, output_index_abs, output_mat_abs):
     if not os.path.isdir(d):
         os.makedirs(d)
 
@@ -71,22 +76,60 @@ def matlab2rst(data, output=None):
     return '\n'.join(header + map(add_indent, res))
 
 
-fns=sum([glob.glob(join(d,'*.m')) for d in [matlab_dir, example_dir]],[])
-fns.sort()
+class RSTType(object):
+    def __init__(self, prefix, build_types):
+        self.prefix=prefix
+        self.build_types=build_types
 
-# define filters + file name infix for toc
-filters={'run': lambda x: x.startswith('run'),
-         None: lambda x: x.startswith('cosmo'),
-         'pb':lambda x: x.startswith('run')}
+    def matches(self, fn):
+        short_fn=split(fn)[1]
+        return short_fn.startswith(self.prefix+'_')
 
-pf='Runnable examples - '
-output2name={('skl','run'):pf + 'skeleton files',
-             (None,'run'):pf + 'full solution files',
-             (None,None): 'Module index',
-             ('hdr',None): 'header files',
-             ('skl',None): 'skeleton files',
-             ('sgn',None): 'signature files',
-             ('None','pb'): 'Matlab outputs'}
+    def matching(self, tp, fns):
+        if tp in self.build_types:
+            return [fn for fn in fns if self.matches(fn)]
+        else:
+            return []
+
+
+    def has_type(self, tp):
+        return tp in self.build_types
+
+    def type2name(self, tp):
+        return {None:'complete files',
+                'skl': 'skeleton files',
+                'hdr': 'header signature files'}[tp]
+
+    def get_name(self):
+        return dict(demo='Demonstrations',
+                    run='Runnable examples',
+                    cosmo='CoSMoMVPA functions')[self.prefix]
+
+    def get_postfix(self):
+        return '' if self.prefix is 'cosmo' else '_'+self.prefix
+
+
+    def needs_full_include(self):
+        return self.prefix=='demo'
+
+class RSTprop(object):
+    def __init__(self):
+        pass
+
+    
+
+
+def base_name(fn, ext=None):
+    if ext is None:
+        ext='.m'
+    
+    p, base_fn=split(fn)
+
+    if not base_fn.endswith(ext):
+        raise ValueError('%s does not end with %s', base_fn, ext)
+
+    return p, base_fn[:-len(ext)]
+
 
 def is_newer(fn, other_fn):
     return not isfile(other_fn) or getmtime(fn)>getmtime(other_fn) 
@@ -94,75 +137,86 @@ def is_newer(fn, other_fn):
     # and other_fn exists
     return 
 
-for output in ('hdr','skl',None,'sgn'):
-    print '@#$====> Converting matlab_%s.m files to *_%s.rst' % (output,output)
-    labels=[]
-    for fn in fns:
-        if split(fn)[-1].startswith('run') and output=='hdr':
-            continue
-        with open(fn) as f:
-            data=f.read()
+rst_types=(RSTType('demo',[None]),
+           RSTType('run',[None,'skl']),
+           RSTType('cosmo',[None,'hdr','skl']))
 
-        rst=matlab2rst(data, output=output)
-        p,fn_short=split(fn)
-        fn_short=fn_short[:-2] # get rid of '.m' extension
-        label=fn_short # how we call it in .rst world
+all_fns=sum([glob.glob(join(d,'*.m')) for d in [matlab_dir, example_dir]],[])
+all_fns.sort()
 
-        if not output is None:
-            label+='_'+output # add suffix
-
-        fn_out=join(trg_dir, rst_sub_dir, label)
-
-        #fn_out+='.rst'
-        fn_out+='.txt' if output=='sgn' else '.rst'
-
-        if is_newer(fn, fn_out):
-            with open(fn_out,'w') as f:
-
-                f.write('.. _%s:\n\n' % label)
-                if output!='sgn':
-                    f.write('%s\n%s\n\n' % (label.replace('_',' '),'-'*len(label)))
-                f.write(rst)
-                #print ">>>", rst, "<<<"
-                #print '%s -> %s' % (fn_short, fn_out)
-
-        labels.append(label) # keep track of all rst files
-
-
-    for infix, filter_ in filters.iteritems():
-        if not (output,infix) in output2name:
+for output in ('hdr','skl',None):
+    infix='' if output is None else '_'+output
+    for rst_type in rst_types:
+        fns=rst_type.matching(output, all_fns)
+        if not len(fns):
             continue
 
-        if output=='sgn':
-            continue
+        base_names=[]
+        rebuild_toc=False
 
-        toc_fn='modindex%s' % ('' if output is None else ('_' + output))
-        fn=toc_fn
-        if not infix is None:
-            fn+='_'+infix
+        for fn in fns:
+            [p,b]=base_name(fn)
+        
+            b+=infix
 
-        headername=output2name.get((output, infix),None)
-        if headername is None:
-            continue
+            # make a text file that can be 'included' in sphinx
+            trg_fn=join(output_mat_abs,'%s.txt' % b)
+            
+            remake_rst=False
+            if is_newer(fn, trg_fn):
+                remake_rst=True
+                with open(fn) as f:
+                    mat=f.read()
 
-        header='%s' % headername
-        header=['.. _`%s`: ' % fn, '', header, '='*len(header),'',
-                'Contents:', '',
-                '.. toctree::','    :maxdepth: 2','','']
+                rst=matlab2rst(mat, output)
+                
+                with open(trg_fn,'w') as f:
+                    f.write(rst)
 
-        appendix=['','',
-                  'Indices and tables',
-                  '^^^^^^^^^^^^^^^^^^',
-                  '',
-                  '* :ref:`genindex`',
-                  '* :ref:`modindex`',
-                  '* :ref:`search`']
-   
-        full_toc_fn=join(modindex_dir,fn+'.rst')
-        with open(full_toc_fn,'w') as f:
-            f.write('\n'.join(header))
-            for label in filter(filter_,labels):
-                f.write(add_indent(rst_sub_dir+'/'+label) + '\n')
-            f.write('\n'.join(appendix))
-            print "Written toc to %s" % full_toc_fn
+            # make the rst file that includes it
+            trg_fn=join(output_mat_abs,'%s.rst' % b)
+            if remake_rst or is_newer(fn, trg_fn):
+                label=b.replace('_',' ')
+                header='.. _%s:\n\n%s\n%s\n\n' % (b,label,'='*len(b))
+                body='.. include :: %s\n\n' % ('%s.txt' % b)
+
+                with open(trg_fn,'w') as f:
+                    f.write(header+body)
+
+                rebuild_toc=True
+
+            
+            base_names.append(b)
+
+        if rebuild_toc:
+            toc_base_name='modindex%s%s' % (infix, rst_type.get_postfix())
+            title='%s - %s' % (rst_type.get_name(), rst_type.type2name(output))
+            header='.. _`%s`:\n\n%s\n%s\n\nContents\n\n.. toctree::\n    :maxdepth: 2\n\n' % (
+                        toc_base_name, title, '='*len(title))
+
+            
+            trg_fn=join(output_root_abs,'%s.rst' % toc_base_name)
+            with open(trg_fn,'w') as f:
+                f.write(header)
+                f.write('\n'.join('    %s/%s' % (output_mat_rel,b) for b in base_names))
+                
+
+            if rst_type.needs_full_include():
+                include_base_name='contents%s.rst' % rst_type.get_postfix()
+                trg_fn=join(output_root_abs,include_base_name)
+
+                title='%s - full listings' % (rst_type.get_name())
+                header=('.. _`%s`:\n\n%s\n%s\n\nContents\n\n.. '
+                        'contents::\n    :local:\n    :depth: 1\n\n') % (
+                        include_base_name, title, '='*len(title))
+
+                body='\n'.join(['%s\n%s\n\n :demo:`%s`\n\n.. include:: %s\n\n\n' % (
+                            b,'+'*len(b),b,join(output_mat_rel,b)+'.txt')
+                                    for b in base_names])
+
+                with open(trg_fn,'w') as f:
+                    f.write(header+body+'\n\n')
+
+
+
 
