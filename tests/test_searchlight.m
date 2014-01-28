be15aa496f897ad93fcf3d028c7cd5184e48b563
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-01-28 19:00:33 +0100

    TST: more tests for searchlight

--- tests/test_searchlight.m
+++ tests/test_searchlight.m
@@ -4,12 +4,24 @@ function test_suite = test_searchlight
 
 function test_searchlight_
 
+    ds=generate_test_dataset();
     measure=@(x,a) cosmo_structjoin('samples',size(x.samples,2));
     m=cosmo_searchlight(ds, measure,'radius',3);
     assertVectorsAlmostEqual(histc(m.samples,[38 46 47 100 123]),...
                             [ 24 24 558 352 35])
 
+                        
+    m=cosmo_searchlight(ds, measure,'radius',-18);
+    assertVectorsAlmostEqual(histc(m.samples,[18 19 20 23 26]),...
+                            [398 495 8 24 76]);
 
+    d=cosmo_slice(ds,mod(ds.fa.i,3)==0,2);
+    m=cosmo_searchlight(d, measure,'radius',-18);
+    assertVectorsAlmostEqual(histc(m.samples,[18 19 21]),[80 56 150]);
+    assertVectorsAlmostEqual(m.samples([100 201]),[21 18]);
+    
+                        
+                        
     % bit of a smoke test
     measure=@cosmo_correlation_measure;
 
