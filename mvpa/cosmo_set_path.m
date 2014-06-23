04be3f9467489a5cb46f244d83a9c2c9e7c13887
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-06-23 15:29:38 +0200

    NF: set_path, to make setting the path easier

---
+++ mvpa/cosmo_set_path.m
@@ -0,0 +1,66 @@
+function cosmo_set_path()
+% set the matlab path for CoSMoMVPA
+%
+% cosmo_set_path()
+%
+% Notes:
+%  - if $ROOT is the root directory of CoSMoMVPA, then this function adds 
+%    the paths $ROOT/{mvpa,external}, and their subdirectories, to the 
+%    matlab path. It removes $ROOT{doc,examples,tests}.
+%  - A warning message is given if an unexpected directory structure is 
+%    encountered 
+%
+% NNO June 2014
+
+    % directories to be added and removed from the path
+    remove_subdirs={'doc','examples','tests'};
+    add_subdirs={'mvpa','external'};
+
+    remove_add_subdirs={remove_subdirs, add_subdirs};
+
+    % get path of this very function
+    me_pth=fileparts(which(mfilename()));
+    
+    % get CoSMoMVPA root path
+    root_pth=fullfile(me_pth,'..');
+
+    % get matlab path, each path in a cell
+    pathsep_=pathsep(); % store path separator
+    matlab_pth=cosmo_strsplit(path(),pathsep_);
+
+    for add=[0,1]
+        subdirs=remove_add_subdirs{add+1};
+
+        n=numel(subdirs);
+        for k=1:n
+            subdir=subdirs{k};
+            full_pth=fullfile(root_pth,subdir);
+
+            if exist(full_pth,'file') && isdir(full_pth)
+                % generate all subdirectories
+                all_pths=cosmo_strsplit(genpath(full_pth),pathsep_);
+                
+                % see which ones are in the matlab path
+                in_matlab_pth=cosmo_match(all_pths,matlab_pth);
+
+                for j=1:numel(all_pths)
+                    pth=all_pths{j};
+                    if add && ~in_matlab_pth(j)
+                        addpath(pth);
+                    elseif ~add && in_matlab_pth(j)
+                        rmpath(pth);
+                    else
+                        % do nothing
+                    end
+                end
+            else
+                warning(['Path ''%s'' not found - this is an unexpectd '...
+                            'directory structure',...
+                            'Please check your path settings'],full_pth);
+            end
+
+        end
+    end
+
+
+
