6432d762d6641700d4cd0b52bd2b89455e1091ae
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-07-05 19:21:55 +0200

    RF+DOC: update documentation and simplify implementation

--- mvpa/cosmo_dataset_slice_fa.m
+++ mvpa/cosmo_dataset_slice_fa.m
@@ -1,46 +1,134 @@
-function dataset=cosmo_dataset_slice_fa(dataset, features_to_select)
-% Slice a dataset by samples, aka rows
+function ds=cosmo_dataset_slice_fa(ds, to_select)
+% Slice a dataset by features (columns)
 %   
-%   dataset = cosmo_dataset_slice_sa(dataset, features_to_select)
+% sliced_ds=cosmo_dataset_slice_fa(ds, to_select)
 %   
-%   Input
-%       dataset: an instance of cosmo_fmri_dataset with N samples
-%       features_to_select:  Either an Nx1 boolean mask, or a vector with 
-%                           indices.
-%   Returns
-%       dataset:    an instance of an fmri_dataset that is a copy of the input dataset
-%                   but contains just the rows indictated in features_to_select, and the 
-%                   corresponding values in feature attributes.
+% Input:
+%   ds          a dataset struct with fields .samples (MxN for M samples
+%               and N features), and optionally 
+%               features attributes in ds.fa, each ?xN)
+%   to_select:  Either a boolean mask, or a vector with indices, indicating
+%               which features to select in ds
 %
-%   Note
-%    - this function is intended as an exercise. For a more powerful 
-%      implementation that deals with cell inputs correctly,
-%      consider using comso_slice(dataset, features_to_select, 2).
+% Returns:
+%   sliced_ds   a dataset struct with the same fields as the input, but 
+%               with the selected samples in .samples (MxP, if to_select
+%               selected P values) and the selected features in ds.fa
+%
+% Examples:
+%     ds=struct();
+%     ds.samples=reshape(1:12,4,3); % 4 samples, 3 features
+%     ds.sa.chunks=[1 1 2 2]';
+%     ds.sa.targets=[1 2 1 2]';
+%     ds.fa.i=[3 8 13];
+%     ds.fa.roi={'vt','loc','v1'};
+%     cosmo_disp(ds);
+%     > .samples
+%     >   [ 1         5         9
+%     >     2         6        10
+%     >     3         7        11
+%     >     4         8        12 ]
+%     > .sa
+%     >   .chunks
+%     >     [ 1
+%     >       1
+%     >       2
+%     >       2 ]
+%     >   .targets
+%     >     [ 1
+%     >       2
+%     >       1
+%     >       2 ]
+%     > .fa
+%     >   .i
+%     >     [  3
+%     >        8
+%     >       13 ]
+%     >   .roi
+%     >     { 'vt'  'loc'  'v1' }
+%     %
+%     % select third and second features (in that order)
+%     sliced_ds=cosmo_dataset_slice_fa(ds, [3 2]);
+%     cosmo_disp(sliced_ds);
+%     > .samples
+%     >   [  9         5
+%     >     10         6
+%     >     11         7
+%     >     12         8 ]
+%     > .sa
+%     >   .chunks
+%     >     [ 1
+%     >       1
+%     >       2
+%     >       2 ]
+%     >   .targets
+%     >     [ 1
+%     >       2
+%     >       1
+%     >       2 ]
+%     > .fa
+%     >   .i
+%     >     [ 13         8 ]
+%     >   .roi
+%     >     { 'v1'  'loc' }
+%     %
+%     % using a logical mask, select features with odd value for .i
+%     msk=mod(ds.fa.i,2)==1;
+%     disp(msk)
+%     > [1 0 1]
+%     sliced_ds=cosmo_dataset_slice_fa(ds, msk);
+%     cosmo_disp(sliced_ds);
+%     > .samples
+%     >   [ 1         9
+%     >     2        10
+%     >     3        11
+%     >     4        12 ]
+%     > .sa
+%     >   .chunks
+%     >     [ 1
+%     >       1
+%     >       2
+%     >       2 ]
+%     >   .targets
+%     >     [ 1
+%     >       2
+%     >       1
+%     >       2 ]
+%     > .fa
+%     >   .i
+%     >     [ 3        13 ]
+%     >   .roi
+%     >     { 'vt'  'v1' }
+%
+% Note:
+%   - this function is intended as an exercise. For a more powerful 
+%     implementation that deals with cell inputs correctly,
+%     consider using comso_slice(dataset, to_select, 2).
 %      
-% See also: cosmo_slice
+% See also: cosmo_slice, cosmo_dataset_slice_fa
     
-    %%
-    % First slice the samples array by rows
+    % First slice the features array by columns
     
     % >@@>
-    dataset.samples=dataset.samples(:,features_to_select);
+    ds.samples=ds.samples(:,to_select);
     % <@@<
     
     %%
-    %   Then go through each of the sample attributes and slice each one.
+    %   If there is a field .fa, go through each attribute and slice it.
     %
-    %   Hint: we used the matlab function 'fieldnames' to list the field in
-    %   dataset.sa in case it is missing either targets or chunk, or in case there
-    %   may be extra unknown sample attributes
+    %   Hint: we used the matlab function 'fieldnames' to list the fields
+    %   in dataset.fa
     
-    % >@@>
-    fns = fieldnames(dataset.sa); 
-    n = numel(fns);
     
-    for k=1:n
-        fn = fns{k};
-        sa = dataset.sa.(fn);
-        dataset.sa.(fn)=sa(:,features_to_select);
+    if isfield(ds,'fa')
+        % >@@>
+        fns = fieldnames(ds.fa); 
+        n = numel(fns);
+
+        for k=1:n
+            fn = fns{k};
+            fa = ds.fa.(fn);
+            ds.fa.(fn)=fa(:,to_select);
+        end
+        % <@@<
     end
-    % <@@<
-    
--- mvpa/cosmo_dataset_slice_sa.m
+++ mvpa/cosmo_dataset_slice_sa.m
@@ -1,45 +1,123 @@
-function dataset=cosmo_dataset_slice_sa(dataset, samples_to_select)
-% Slice a dataset by samples, aka rows
+function ds=cosmo_dataset_slice_sa(ds, to_select)
+% Slice a dataset by samples (rows)
 %   
-%   dataset = cosmo_dataset_slice_sa(dataset, samples_to_select)
+% sliced_ds=cosmo_dataset_slice_sa(ds, to_select)
 %   
-%   Input
-%       dataset: an instance of cosmo_fmri_dataset with N samples
-%       samples_to_select:  Either an Nx1 boolean mask, or a vector with 
-%                           indices.
-%   Returns
-%       dataset:    an instance of an fmri_dataset that is a copy of the input dataset
-%                   but contains just the rows indictated in sample_indices, and the 
-%                   corresponding values in sample attributes.
-%   Note
-%    - this function is intended as an exercise. For a more powerful 
-%      implementation that deals with cell inputs correctly,
-%      consider using comso_slice(dataset, features_to_select).
+% Input:
+%   ds          a dataset struct with fields .samples (MxN for M samples
+%               and N features), and optionally 
+%               sample attributes in ds.fa, each Nx>)
+%   to_select:  Either a boolean mask, or a vector with indices, indicating
+%               which samples to select in ds
+%
+% Returns:
+%   sliced_ds   a dataset struct with the same fields as the input, but 
+%               with the selected samples in .samples (PxN, if to_select
+%               selected O values) and the selected features in ds.sa
+%
+% Examples:
+%     ds=struct();
+%     ds.samples=reshape(1:12,4,3); % 4 samples, 3 features
+%     ds.sa.chunks=[1 1 2 2]';
+%     ds.sa.targets=[1 2 1 2]';
+%     ds.fa.i=[3 8 13];
+%     ds.fa.roi={'vt','loc','v1'};
+%     cosmo_disp(ds);
+%     > .samples
+%     >   [ 1         5         9
+%     >     2         6        10
+%     >     3         7        11
+%     >     4         8        12 ]
+%     > .sa
+%     >   .chunks
+%     >     [ 1
+%     >       1
+%     >       2
+%     >       2 ]
+%     >   .targets
+%     >     [ 1
+%     >       2
+%     >       1
+%     >       2 ]
+%     > .fa
+%     >   .i
+%     >     [ 3         8        13 ]
+%     >   .roi
+%     >     { 'vt'  'loc'  'v1' }
+%     %
+%     % select third and second samples (in that order)
+%     sliced_ds=cosmo_dataset_slice_sa(ds, [3 2]);
+%     cosmo_disp(sliced_ds);
+%     > .samples
+%     >   [ 3         7        11
+%     >     2         6        10 ]
+%     > .sa
+%     >   .chunks
+%     >     [ 2
+%     >       1 ]
+%     >   .targets
+%     >     [ 1
+%     >       2 ]
+%     > .fa
+%     >   .i
+%     >     [ 3         8        13 ]
+%     >   .roi
+%     >     { 'vt'  'loc'  'v1' }
+%     %
+%     % using a logical mask, select samples with odd value for .targets
+%     msk=mod(ds.sa.targets,2)==1;
+%     disp(msk)
+%     >      1
+%     >      0
+%     >      1
+%     >      0
+%     sliced_ds=cosmo_dataset_slice_sa(ds, msk);
+%     cosmo_disp(sliced_ds);
+%     > .samples
+%     >   [ 1         5         9
+%     >     3         7        11 ]
+%     > .sa
+%     >   .chunks
+%     >     [ 1
+%     >       2 ]
+%     >   .targets
+%     >     [ 1
+%     >       1 ]
+%     > .fa
+%     >   .i
+%     >     [ 3         8        13 ]
+%     >   .roi
+%     >     { 'vt'  'loc'  'v1' }
+%
+% Note:
+%   - this function is intended as an exercise. For a more powerful 
+%     implementation that deals with cell inputs correctly,
+%     consider using comso_slice(dataset, to_select, 1).
 %      
-% See also: cosmo_slice
+% See also: cosmo_slice, cosmo_dataset_slice_fa
     
-    %%
-    % First slice the samples array by rows
+    % First slice the features array by columns
     
     % >@@>
-    dataset.samples=dataset.samples(samples_to_select,:);
+    ds.samples=ds.samples(to_select,:);
     % <@@<
     
     %%
-    %   Then go through each of the sample attributes and slice each one.
+    %   If there is a field .sa, go through each attribute and slice it.
     %
-    %   Hint: we used the matlab function 'fieldnames' to list the field in
-    %   dataset.sa in case it is missing either targets or chunk, or in case there
-    %   may be extra unknown sample attributes
+    %   Hint: we used the matlab function 'fieldnames' to list the fields
+    %   in dataset.fa
     
-    % >@@>
-    fns = fieldnames(dataset.sa); 
-    n = numel(fns);
     
-    for k=1:n
-        fn = fns{k};
-        sa = dataset.sa.(fn);
-        dataset.sa.(fn)=sa(samples_to_select,:);
+    if isfield(ds,'sa')
+        % >@@>
+        fns = fieldnames(ds.sa); 
+        n = numel(fns);
+
+        for k=1:n
+            fn = fns{k};
+            sa = ds.sa.(fn);
+            ds.sa.(fn)=sa(to_select,:);
+        end
+        % <@@<
     end
-    % <@@<
-    
