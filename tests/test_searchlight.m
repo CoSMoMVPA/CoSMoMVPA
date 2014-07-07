595406fc37f92524a7192850cda4cefd2518e288
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-07-07 17:18:07 +0200

    TST: do not show progress in unit test

--- tests/test_searchlight.m
+++ tests/test_searchlight.m
@@ -6,17 +6,17 @@ function test_searchlight_
 
     ds=generate_test_dataset();
     measure=@(x,a) cosmo_structjoin('samples',size(x.samples,2));
-    m=cosmo_searchlight(ds, measure,'radius',3);
+    m=cosmo_searchlight(ds, measure,'radius',3,'progress',0);
     assertVectorsAlmostEqual(histc(m.samples,[38 46 47 100 123]),...
                             [ 24 24 558 352 35])
 
                         
-    m=cosmo_searchlight(ds, measure,'radius',-18);
+    m=cosmo_searchlight(ds, measure,'radius',-18,'progress',0);
     assertVectorsAlmostEqual(histc(m.samples,[18 19 20 23 26]),...
                             [398 495 8 24 76]);
 
     d=cosmo_slice(ds,mod(ds.fa.i,3)==0,2);
-    m=cosmo_searchlight(d, measure,'radius',-18);
+    m=cosmo_searchlight(d, measure,'radius',-18,'progress',0);
     assertVectorsAlmostEqual(histc(m.samples,[18 19 21]),[80 56 150]);
     assertVectorsAlmostEqual(m.samples([100 201]),[21 18]);
     
@@ -25,7 +25,8 @@ function test_searchlight_
     % bit of a smoke test
     measure=@cosmo_correlation_measure;
 
-    nbrhood=cosmo_spherical_neighborhood(ds,3);
-    m=cosmo_searchlight(ds, measure,'nbrhood',nbrhood,'center_ids',[100 201]);
+    nbrhood=cosmo_spherical_neighborhood(ds,3,cosmo_structjoin('progress',0));
+    m=cosmo_searchlight(ds, measure,'nbrhood',nbrhood,...
+                            'center_ids',[100 201],'progress',0);
 
     assertVectorsAlmostEqual(m.samples,[0.0014 0.0204],'relative',.001);
