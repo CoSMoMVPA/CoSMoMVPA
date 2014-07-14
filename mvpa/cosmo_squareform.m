41012ed2fece8201bc91b1b02f40cc54eef5bae0
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-07-14 14:19:58 +0200

    NF+EXT: custom squareform implementation to rely less on stats toolbox

---
+++ mvpa/cosmo_squareform.m
@@ -0,0 +1,110 @@
+function s=cosmo_squareform(x, direction)
+% converts pair-wise distances between matrix and vector form
+%
+% s=cosmo_squareform(x[, direction])
+%
+% Inputs:
+%    x           One of:
+%                - NxN distance matrix; x must be symmetric and have zeros
+%                  on the diagonal
+%                - 1xM distance vector
+%    direction   Optional. If provided it must be 'tovector' (if x is a
+%                matrix) or 'tomatrix' (if x is a vector). If not provided
+%                it is set to 'tovector' if x is a matrix and to 'tomatrix'
+%                if x is a vector
+% 
+% Returns:
+%    s           One of:
+%                - NxN distance matrix, if direction=='tomatrix'
+%                - 1xM distance vector, if direction=='tovector'
+%                it must hold that N*(N-1)/2=M
+%
+% Notes:
+%  - this function provides the same functionality as the built-in function
+%    ''squareform'' in the matlab stats toolbox.
+%
+% NNO Jul 2014
+
+
+if nargin<2
+    if numel(size(x))~=2
+        error('Input must be matrix or vector');
+    end
+    
+    if isvector(x) 
+        direction='tomatrix';
+    else
+        direction='tovector';
+    end
+end
+
+if isempty(x)
+    s=x;
+end
+
+switch direction
+    case 'tomatrix'
+        if ~isvector(x)
+            error('direction ''%s'' requires a vector as input',direction);
+        end
+        n=numel(x);
+        
+        % side*(side+1)/2=n, solve for side>0
+        side=(1+sqrt(1+8*n))/2;
+        if ~isequal(side, round(side))
+            error(['size %d of input vector is not correct for '...
+                    'the number of elements below diagonal of a '...
+                    'square matrix'], n);
+        end
+        msk=bsxfun(@gt,(1:side)',1:side);
+        
+        if islogical(x)
+            s=false(side);
+            s(msk)=x;
+            s=s|s';
+        elseif isnumeric(x)
+            s=zeros(side);
+            s(msk)=x;
+            s=s+s';
+        else
+            error('Unsupported data type ''%s''', class(x));
+        end
+        
+    case 'tovector'
+        [side,side_]=size(x);
+        if side~=side_
+            error('direction ''%s'' requires a square matrix as input', ...
+                                                        direction);
+        end
+        
+        if ~(islogical(x) || isnumeric(x))
+            error('Unsupported data type ''%s''', class(x));
+        end
+        
+        dg=diag(x);
+        if any(dg)
+            error('square matrix must be all zero on diagonal');
+        end
+        
+        if ~isequal(x,x');
+            error('square matrix must be symmetric');
+        end
+        
+        msk=bsxfun(@gt,(1:side)',1:side);
+        
+        s=x(msk);    
+        s=s(:)';
+           
+    otherwise
+        error(['illegal direction argument ''%s'', must be one of ', ...
+                    '''tomatrix'',''tovector'''],direction);
+        
+end
+        
+            
+            
+            
+        
+        
+        
+        
---
+++ tests/test_squareform.m
@@ -0,0 +1,38 @@
+function test_suite = test_squareform
+    initTestSuite;
+
+
+function test_squareform_()
+    vec=1:6;
+    mx=[0 1 2 3; 1 0 4 5; 2 4 0 6; 3 5 6 0];
+    
+    sf=@cosmo_squareform;
+
+
+    assertElementsAlmostEqual(sf(vec),mx);
+    assertElementsAlmostEqual(sf(vec),mx,'tomatrix');
+    assertElementsAlmostEqual(sf(mx),vec);
+    assertElementsAlmostEqual(sf(mx),vec,'tovector');
+
+    assertExceptionThrown(@() sf(vec,'foo'),'');
+    assertExceptionThrown(@() sf(mx,'foo'),'');
+    assertExceptionThrown(@() sf(vec,struct()),...
+                                    'MATLAB:badSwitchExpression');
+    assertExceptionThrown(@() sf(struct()),'');
+    assertExceptionThrown(@() sf(struct(),'tovector'),'');
+    assertExceptionThrown(@() sf(struct(),'tomatrix'),'');
+    assertExceptionThrown(@() sf(zeros([2,2,2])),'');
+    assertExceptionThrown(@() sf(zeros([2,2,2]),'tovector'),'');
+    assertExceptionThrown(@() sf(zeros([2,2,2]),'tomatrix'),'');
+
+    assertExceptionThrown(@() sf([vec 1]),'');
+    assertExceptionThrown(@() sf([vec 1],'tomatrix'),'');
+
+    assertExceptionThrown(@() sf(eye(4)+mx),'');
+    assertExceptionThrown(@() sf(eye(4)+mx,'tovector'),'');
+
+    mx(2,1)=3;
+    assertExceptionThrown(@() sf(mx,'tovector'),'');
+    
+
+
