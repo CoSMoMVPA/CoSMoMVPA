3f3c7afa2868d25851aebfcf63737de4d507934d
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-08-06 20:54:19 +0200

    RF: do publishing in pwd

--- mvpa/cosmo_publish_run_scripts.m
+++ mvpa/cosmo_publish_run_scripts.m
@@ -11,25 +11,17 @@ function cosmo_publish_run_scripts(force)
 %                published again. If true, all run_* scripts are published
 %                (rebuilt)
 %
-% Notes to developers:
-% - for this function to function properly, it is required that
-%   comso_get_data_path returns an absolute path. This is because this
-%   function makes copies of the run_* scripts in a temporary directory
-%   with lines containing '% >@@>' or '% <@@<' removed, and scripts are run
-%   from that directory. When cosmo_get_data_path does not return an
-%   absolute path then it may be unable to find the comso example data.
 %
 
-
+    % save original working directory
     pdir=pwd();
-
     c=onCleanup(@()cd(pdir));
 
     medir=fileparts(which(mfilename()));
     cd(medir);
 
-    srcdir=fullfile(pdir,'../examples/');
-    trgdir=fullfile(pdir,'..//doc/source/_static/publish/');
+    srcdir=fullfile(medir,'../examples/');
+    trgdir=fullfile(medir,'..//doc/source/_static/publish/');
 
     if isunix()
         tmpdir='/tmp';
@@ -60,17 +52,17 @@ function cosmo_publish_run_scripts(force)
     outputs=cell(nsrc,1);
 
     p=path();
-    has_pwd=~isempty(strfind(p,pdir));
+    has_pwd=~isempty(strfind(p,medir));
     if ~has_pwd
-        addpath(pdir)
+        addpath(medir)
     end
 
     output_pos=0;
     for k=1:nsrc
-        cd(pdir);
+        cd(medir);
         update=true;
         srcfn=fullfile(srcdir,srcfns(k).name);
-        [unused,srcnm,unused]=fileparts(srcfn);
+        [srcpth,srcnm,unused]=fileparts(srcfn);
         for j=1:ntrg
             [unused,trgnm,unused]=fileparts(trgfns(j).name);
             if ~force && strcmp(srcnm, trgnm) && ...
@@ -89,9 +81,10 @@ function cosmo_publish_run_scripts(force)
         end
 
         fprintf('building: %s ...', srcnm);
-        tmpfn=fullfile(tmpdir,srcfns(k).name);
-        remove_annotation(srcfn, tmpfn);
-        cd(tmpdir);
+        %tmpfn=fullfile(tmpdir,srcfns(k).name);
+        %remove_annotation(srcfn, tmpfn);
+        %cd(tmpdir);
+        cd(srcpth);
         is_built=false;
         try
             publish(srcnm, struct('outputDir',trgdir,'catchError',false));
@@ -105,7 +98,7 @@ function cosmo_publish_run_scripts(force)
             warning('Unable to build %s%s: %s', srcnm, srcext, me.message);
             fprintf('%s\n', me.getReport);
         end
-        cd(pdir);
+        cd(medir);
 
         if ~is_built
             continue
@@ -117,11 +110,12 @@ function cosmo_publish_run_scripts(force)
     end
 
     if ~has_pwd
-        rmpath(pdir);
+        rmpath(medir);
     end
 
     outputfn=fullfile(trgdir, summaryfn);
     fid=fopen(outputfn,'w');
+    c_=onCleanup(@()fclose(fid));
     fprintf(fid,['<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"'...
             '>\n']);
     fprintf(fid,'<HTML><HEAD><TITLE>Index of matlab outputs</TITLE></HEAD>\n');
@@ -132,19 +126,20 @@ function cosmo_publish_run_scripts(force)
         fprintf(fid,'<LI><A HREF="%s%s">%s</A></LI>\n',nm,trgext,nm);
     end
     fprintf(fid,'</UL>Back to <A HREF="../../index.html">index</A>.</BODY></HTML>\n');
-    fclose(fid);
-
     fprintf('Index written to %s\n', outputfn);
 
 
-    function remove_annotation(srcfn,trgfn)
-
+function remove_annotation(srcfn,trgfn)
+    srcfn
+    trgfn
     if strcmpi(srcfn,trgfn)
         error('source and target are the same: %s', srcfn);
     end
 
     fid=fopen(srcfn);
+    c=onCleanup(@()fclose(fid));
     wid=fopen(trgfn,'w');
+    c_=onCleanup(@()fclose(wid));
 
     while true
         line=fgetl(fid);
@@ -152,9 +147,11 @@ function cosmo_publish_run_scripts(force)
             break
         end
 
-        if startswith(line,'% >@@>') || startswith(line,'% <@@<')
-            continue
-        end
+        % For now remove annotation is disabled
+        % XXX should it be re-enabled
+        % if startswith(line,'% >@@>') || startswith(line,'% <@@<')
+        %     continue
+        % end
 
         fprintf(wid,'%s\n',line);
     end
@@ -163,7 +160,7 @@ function cosmo_publish_run_scripts(force)
     fclose(wid);
 
 
-    function s=startswith(haystack, needle)
+function s=startswith(haystack, needle)
 
     t=strtrim(haystack);
     n=numel(needle);
