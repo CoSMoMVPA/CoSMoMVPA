b0edf2d2932777a344b35a9831c6d249ba80c209
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-08-24 21:05:18 +0200

    NF: k-nearest neighbor classifier

---
+++ mvpa/cosmo_classify_knn.m
@@ -0,0 +1,84 @@
+function predicted=cosmo_classify_knn(samples_train, targets_train, samples_test, opt)
+% nearest neighbor classifier
+%
+% predicted=cosmo_classify_nn(samples_train, targets_train, samples_test[, opt])
+%
+% Inputs:
+%   samples_train      PxR training data for P samples and R features
+%   targets_train      Px1 training data classes
+%   samples_test       QxR test data
+%   opt                struct with field:
+%     .knn             Number of nearest neighbors to be considered
+%
+% Output:
+%   predicted          Qx1 predicted data classes for samples_test
+%
+% Example:
+%     ds=cosmo_synthetic_dataset('ntargets',5,'nchunks',15);
+%     test_chunk=1;
+%     te=cosmo_slice(ds,ds.sa.chunks==test_chunk);
+%     tr=cosmo_slice(ds,ds.sa.chunks~=test_chunk);
+%     opt=struct();
+%     opt.knn=2;
+%     pred=cosmo_classify_knn(tr.samples,tr.sa.targets,te.samples,opt);
+%     % show targets and predicted labels (40% accuracy)
+%     disp([te.sa.targets pred])
+%     >      1     4
+%     >      2     2
+%     >      3     5
+%     >      4     1
+%     >      5     5
+%
+% Notes:
+%   - in the case of knn=1, this function is identical to cosmo_classify_nn
+%
+% See also: cosmo_crossvalidate, cosmo_crossvalidation_measure
+%
+% NNO Aug 2013
+
+    cosmo_isfield(opt,'knn',true);
+    knn=opt.knn;
+
+    if cosmo_isfield(opt,'norm')
+        norm_=opt.norm;
+    else
+        norm_=2;
+    end
+
+    [ntrain, nfeatures]=size(samples_train);
+    [ntest, nfeatures_]=size(samples_test);
+    ntrain_=numel(targets_train);
+
+    if knn>nfeatures
+        error('Cannot find nearest %d neighbors: only %d features',...
+                    knn, nfeatures);
+    end
+
+    if nfeatures~=nfeatures_ || ntrain_~=ntrain, error('illegal input size'); end
+
+    % allocate space for output
+    all_predicted=zeros(ntest, knn);
+
+    % classify each test sample
+    for k=1:ntest
+        % for each sample in the test set:
+        % - compute its  distance to each sample in the train set.
+        % - assign the class label of the feature that is nearest
+        % >@@>
+        delta=bsxfun(@minus, samples_train, samples_test(k,:));
+
+        pow_distance=sum(delta.^norm_,2);
+
+        [unused, i]=sort(pow_distance);
+        all_predicted(k, 1:knn)=i(1:knn);
+        % <@@<
+    end
+
+    % determine which targets are predicted most often
+    [winners,classes]=cosmo_winner_indices(targets_train(all_predicted));
+    predicted=classes(winners);
+
+
+
+
+
