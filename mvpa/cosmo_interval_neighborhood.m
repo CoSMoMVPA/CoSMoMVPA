46d618a9bdcfa564428a4a9e1bcbd9f26ac87591
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-09-12 10:57:52 +0200

    TST+BF: properly store fdim, and add doctest

--- mvpa/cosmo_interval_neighborhood.m
+++ mvpa/cosmo_interval_neighborhood.m
@@ -17,81 +17,57 @@ function nbrhood=cosmo_interval_neighborhood(ds, label, radius)
 %                 where the indices differ at most radius from each other.
 %
 % Examples
-%     % ds is an MEEG dataset with a time dimension
-%     ds=cosmo_synthetic_dataset('type','meeg','size','big');
-%     %
-%     % neighborhoods with time bins 7 (=3*2+1) timepoints wide
-%     % (every neighborhood contains all channels)
-%     nbrhood=cosmo_interval_neighborhood(ds,'time',3);
-%     cosmo_disp(nbrhood);
-%     > .a
-%     >   .fdim
-%     >     { [ -0.2     -0.15      -0.1  ...  0      0.05       0.1 ]@1x7 }
-%     >   .meeg
-%     >     .samples_type
-%     >       'timelock'
-%     >     .samples_field
-%     >       'trial'
-%     >     .samples_label
-%     >       'rpt'
-%     >   .hdr_ft
-%     > .fa
-%     >   .time
-%     >     [ 1 2 3  ...  5 6 7 ]@1x7
-%     > .neighbors
-%     >   { [ 1 2 3  ...  1.22e+03  1.22e+03  1.22e+03 ]@1x1224
-%     >     [ 1 2 3  ...  1.53e+03  1.53e+03  1.53e+03 ]@1x1530
-%     >     [ 1 2 3  ...  1.83e+03  1.84e+03  1.84e+03 ]@1x1836
-%     >                                      :
-%     >     [ 307 308 309  ...  2.14e+03  2.14e+03  2.14e+03 ]@1x1836
-%     >     [ 613 614 615  ...  2.14e+03  2.14e+03  2.14e+03 ]@1x1530
-%     >     [ 919 920 921  ...  2.14e+03  2.14e+03  2.14e+03 ]@1x1224 }@7x1
-%     %
-%     % neighborhoods containing just each timepoint itself
-%     nbrhood=cosmo_interval_neighborhood(ds,'time',0);
-%     cosmo_disp(nbrhood.neighbors)
-%     > { [ 1         2         3    ...  304       305       306 ]@1x306
-%     >   [ 307       308       309  ...  610       611       612 ]@1x306
-%     >   [ 613       614       615  ...  916       917       918 ]@1x306
-%     >                                      :
-%     >   [ 1.22e+03  1.23e+03  1.23e+03  ...  1.53e+03  1.53e+03  1.53e+03 ]@1x306
-%     >   [ 1.53e+03  1.53e+03  1.53e+03  ...  1.83e+03  1.84e+03  1.84e+03 ]@1x306
-%     >   [ 1.84e+03  1.84e+03  1.84e+03  ...  2.14e+03  2.14e+03  2.14e+03 ]@1x306 }@7x1
-%
-%     % Illustrate the 'freq' dimension in MEEG time-frequency dataset
+%     % Illustrate the 'time' dimension in MEEG time-frequency dataset,
 %     ds=cosmo_synthetic_dataset('type','timefreq','size','big');
 %     %
-%     % Neighborhoods with 5 (=2*2+1) elements wide
+%     % neighborhoods with bins 5 (=2*1+1) frequency bands wide
+%     % (every neighborhood contains all channels and time points)
 %     nbrhood=cosmo_interval_neighborhood(ds,'freq',2);
-%     cosmo_disp(nbrhood)
-%     > .a
-%     >   .fdim
-%     >     { [ 2         4         6  ...  10        12        14 ]@1x7 }
-%     >   .meeg
-%     >     .samples_type
-%     >       'timefreq'
-%     >     .samples_field
-%     >       'powspctrm'
-%     >     .samples_label
-%     >       'rpt'
-%     >   .hdr_ft
-%     > .fa
-%     >   .freq
-%     >     [ 1         2         3  ...  5         6         7 ]@1x7
-%     > .neighbors
-%     >   { [ 1         2         3  ...  9.48e+03  9.48e+03  9.49e+03 ]@1x4590
-%     >     [ 1         2         3  ...  9.79e+03  9.79e+03  9.79e+03 ]@1x6120
-%     >     [ 1         2         3  ...  1.01e+04  1.01e+04  1.01e+04 ]@1x7650
-%     >                                         :
-%     >     [ 613       614       615  ...  1.07e+04  1.07e+04  1.07e+04 ]@1x7650
-%     >     [ 919       920       921  ...  1.07e+04  1.07e+04  1.07e+04 ]@1x6120
-%     >     [ 1.22e+03  1.23e+03  1.23e+03  ...  1.07e+04  1.07e+04  1.07e+04 ]@1x4590 }@7x1
+%     cosmo_disp(nbrhood.a.fdim)
+%     > .labels
+%     >   { 'freq' }
+%     > .values
+%     >   { [ 2         4         6  ...  10        12        14 ]@1x7 }
+%     cosmo_disp(nbrhood.fa.freq)
+%     > [ 1         2         3  ...  5         6         7 ]@1x7
+%     cosmo_disp(nbrhood.neighbors)
+%     > { [ 1         2         3  ...  9.48e+03  9.48e+03  9.49e+03 ]@1x4590
+%     >   [ 1         2         3  ...  9.79e+03  9.79e+03  9.79e+03 ]@1x6120
+%     >   [ 1         2         3  ...  1.01e+04  1.01e+04  1.01e+04 ]@1x7650
+%     >                                       :
+%     >   [ 613       614       615  ...  1.07e+04  1.07e+04  1.07e+04 ]@1x7650
+%     >   [ 919       920       921  ...  1.07e+04  1.07e+04  1.07e+04 ]@1x6120
+%     >   [ 1.22e+03  1.23e+03  1.23e+03  ...  1.07e+04  1.07e+04  1.07e+04 ]@1x4590 }@7x1
+%
+%     % ds is an MEEG dataset with a time dimension
+%     ds=cosmo_synthetic_dataset('type','timelock','size','big');
+%     %
+%     % Neighborhoods just the frequency bin itself
+%     % (every neighborhood contains all channels)
+%     nbrhood=cosmo_interval_neighborhood(ds,'time',2);
+%     cosmo_disp(nbrhood.a.fdim)
+%     > .labels
+%     >   { 'time' }
+%     > .values
+%     >   { [ -0.2     -0.15      -0.1  ...  0      0.05       0.1 ]@1x7 }
+%     cosmo_disp(nbrhood.fa.time)
+%     > [ 1         2         3  ...  5         6         7 ]@1x7
+%     cosmo_disp(nbrhood.neighbors)
+%     > { [ 1         2         3  ...  916       917       918 ]@1x918
+%     >   [ 1         2         3  ...  1.22e+03  1.22e+03  1.22e+03 ]@1x1224
+%     >   [ 1         2         3  ...  1.53e+03  1.53e+03  1.53e+03 ]@1x1530
+%     >                                      :
+%     >   [ 613       614       615  ...  2.14e+03  2.14e+03  2.14e+03 ]@1x1530
+%     >   [ 919       920       921  ...  2.14e+03  2.14e+03  2.14e+03 ]@1x1224
+%     >   [ 1.22e+03  1.23e+03  1.23e+03  ...  2.14e+03  2.14e+03  2.14e+03 ]@1x918 }@7x1
+%
 %
 % Notes:
-%   - to combine neighborhoods from different dimensions, use
-%     cosmo_neighborhood
+%   - to combine neighborhoods from different dimensions (such as
+%     time, freq, chan, use cosmo_neighborhood
+%   - the output can be used for a searchlight using cosmo_searchlight
 %
-% See also: cosmo_neighborhood
+% See also: cosmo_neighborhood, cosmo_searchlight
 %
 % NNO Feb 2014
 
@@ -127,8 +103,8 @@ function nbrhood=cosmo_interval_neighborhood(ds, label, radius)
     nbrhood.a=ds.a;
 
     a_dim=struct();
-    a_dim={label};
-    a_dim={dim_values};
+    a_dim.labels={label};
+    a_dim.values={dim_values};
     nbrhood.a.(dim_name)=a_dim;
 
     nbrhood.(attr_name)=struct();
