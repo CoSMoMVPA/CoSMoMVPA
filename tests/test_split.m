a6df3f35d94777d0a11285818b9186857e71f913
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-10-02 11:38:52 +0200

    TST: unit test for "split"

---
+++ tests/test_split.m
@@ -0,0 +1,56 @@
+function test_suite = test_split
+    initTestSuite;
+
+
+function test_split_()
+    ds=cosmo_synthetic_dataset();
+    sp1=cosmo_split(ds,'chunks');
+
+    for k=1:numel(sp1)
+        s=sp1{k};
+        assertEqual(s, cosmo_slice(ds,(k*2+[-1 0])));
+        assert(all(s.sa.chunks==k));
+    end
+
+    sp2=cosmo_split(ds,'targets');
+
+    for k=1:numel(sp2)
+        s=sp2{k};
+        assertEqual(s, cosmo_slice(ds,k+[0 2 4]));
+    end
+
+    sp3=cosmo_split(ds,{'targets'});
+    assertEqual(sp2,sp3);
+
+    sp4=cosmo_split(ds,{'targets'},1);
+    assertEqual(sp2,sp4);
+
+    sp5=cosmo_split(ds,'i',2);
+
+    for k=1:numel(sp5)
+        s=sp5{k};
+        assertEqual(s, cosmo_slice(ds,k+[0 3],2));
+    end
+
+    sp6=cosmo_split(ds,{'chunks','targets'});
+    for k=1:numel(sp5)
+        s=sp6{k};
+        assertEqual(s, cosmo_slice(ds,k,1));
+    end
+
+    sp7=cosmo_split(ds,{'j','i','k'},2);
+    for k=1:numel(sp5)
+        s=sp7{k};
+        assertEqual(s, cosmo_slice(ds,k,2));
+    end
+
+    assertEqual(cosmo_split(ds,[],1),{ds});
+    assertEqual(cosmo_split(ds,[],2),{ds});
+
+    aet=@(x,y)assertExceptionThrown(@()cosmo_split(ds,x,y),'');
+    aet('i',1);
+    aet('chunks',2);
+    aet('chunks',3);
+
+    ds.sa.targets=[ds.sa.targets ds.sa.targets];
+    assertExceptionThrown(@()cosmo_split(ds,'targets',1),'');
