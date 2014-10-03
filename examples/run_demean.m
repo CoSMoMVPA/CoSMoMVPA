0884937759ff97f88229b7e43ed76256c33e788e
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-10-03 11:38:23 +0200

    ENH: update demean example

--- examples/run_demean.m
+++ examples/run_demean.m
@@ -2,20 +2,41 @@
 %
 
 %% Generate random dataset
-ds=struct();
-ds.samples=(rand(20,12));
-subplot(1,2,1);
-imagesc(ds.samples,[-1 1])
-colorbar();
-title('before de-meaning');
+ds=cosmo_synthetic_dataset('nchunks',4,'ntargets',3);
 
-%% Demean the dataset
+%% Split the dataset by chunks
 % >@@>
-ds.samples = bsxfun(@minus, ds.samples, mean(ds.samples,1));
-% <@@<
-subplot(1,2,2);
-imagesc(ds.samples,[-1 1])
-colorbar();
-title('after de-meaning');
+splits=cosmo_split(ds,{'chunks'},1);
+% >@@>
+nsplits=numel(splits);
 
+% allocate space for output
+outputs=cell(nsplits,1);
 
+% treat each element in splits seperately, and subtract the mean for each
+% feature seperately
+for k=1:nsplits
+    d=splits{k};
+    % >@@>
+
+    % mean over samples, for each feature
+    mu=mean(d.samples,1);
+
+    % subtract the mean.
+    % equivalent, but less efficient, is:
+    %     nsamples=size(d.samples,1);
+    %     d.samples=d.samples-repmat(mu,nsamples,1);
+    %
+    d.samples=bsxfun(@minus,d.samples,mu);
+    % >@@>
+
+    % store output
+    outputs{k}=d;
+end
+
+ds_demeaned=cosmo_stack(outputs);
+
+%% Alternative approach to demeaning
+
+demeaner=@(x)bsxfun(@minus,x,mean(x,1)); % function handle as helper
+ds_demeaned_alt=cosmo_fx(ds,demeaner,'chunks');
