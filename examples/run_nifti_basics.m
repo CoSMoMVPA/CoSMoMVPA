323145e7fbcc072fe6c7ef70378329b585d6f421
Jens Schwarzbach <jens.schwarzbach@unitn.it>
2014-10-04 16:16:19 +0200

    WEB+EXC: update nifti dataset basics

--- examples/run_nifti_basics.m
+++ examples/run_nifti_basics.m
@@ -6,7 +6,7 @@ config=cosmo_config();
 data_path=fullfile(config.tutorial_data_path,'ak6','s01');
 
 % Set filename
-fn=[data_path '/brain.nii'];
+fn=fullfile(data_path, 'brain.nii');
 
 % Load with nifti
 ni=load_nii(fn);
@@ -18,38 +18,63 @@ cosmo_disp(ni.hdr);
 size(ni.img)
 
 % plot a histogram of the intensities (use only values greater than zero)
+% change the number of bins
 % >@@>
-hist(ni.img(ni.img>0),100)
+figure
+set(gcf, 'name', 'Intensity Histogram')
+hist(ni.img(ni.img(:) > 0), 100)
+xlabel('intensity')
+ylabel('count')
+box off
 % <@@<
-
 %% Plot slices
 % plot a sagital, coronal and axial slice
 % at voxel positions (80,150,80) using squeeze and tranpose ("'") where
 % necessary.
-% (bonus points for proper orientations)
+% (bonus points for axis labels and proper orientations, i.e. in the 
+% sagittal view the front of the brain is on the left and the back
+% is on the right)
 ii=80;
 jj=150;
 kk=80;
 figure
 % >@@>
+set(gcf, 'name', 'Canonical Views')
 subplot(2,2,1)
+%imagesc(fliplr(rot90(squeeze(ni.img(ii, :, :)))))
 imagesc(squeeze(ni.img(ii,end:-1:1,end:-1:1))')
-subplot(2,2,2)
-imagesc(squeeze(ni.img(:,jj,end:-1:1))')
-subplot(2,2,3)
-imagesc(squeeze(ni.img(:,end:-1:1,kk)))
-% <@@<
+axis image
+title('SAG')
+xlabel('y [AP]')
+ylabel('z [SI]')
 
+subplot(2,2,2)
+%imagesc(fliplr(rot90(squeeze(ni.img(:, jj, :)))))
+imagesc(squeeze(ni.img(:,jj,end:-1:1))')
+axis image
+title('COR')
+xlabel('x [LR]')
+ylabel('z [SI]')
+
+subplot(2,2,3)
+%imagesc(fliplr(rot90(ni.img(:, :, kk))))
+imagesc(squeeze(ni.img(:,end:-1:1,kk))')
+axis image
+title('TRA')
+xlabel('x [LR]')
+ylabel('y [AP]')
+% <@@<
 %% Plot slice in all three dimensions
 % This uses the cosmo_plot_slices helper function
 slice_step=15;
+strView = {'SAG', 'COR', 'TRA'};
 
 for dim=1:3
     figure
     % >@@>
-    % make a new figure
-    cosmo_plot_slices(ni.img,dim,slice_step);
+    cosmo_plot_slices(ni.img, dim, slice_step)
+    axh = findobj(gcf, 'Type', 'axes');
+    set(axh, 'visible', 'off')
+    set(gcf, 'name', sprintf('Slices %s', strView{dim}));
     % <@@<
 end
-
-
