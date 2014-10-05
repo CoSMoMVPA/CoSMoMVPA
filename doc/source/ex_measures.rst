1dcdda4b4c87c18658f01286408382b7867c70b3
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-10-05 20:09:41 +0200

    NF+EXC+WEB: new exercise illustrating CoSMoMVPA measures

---
+++ doc/source/ex_measures.rst
@@ -0,0 +1,66 @@
+.. _`ex_classify_lda`:
+
+Using CoSMoMVPA *measures*
+==========================
+
+Rationale
++++++++++
+
+Previous exercises discussed how to compute both split-half correlations (:ref:`ex_splithalf_correlations`) and classification accuracies using cross-validation (:ref:`ex_nfold_crossvalidation`). Both types of analysis share a common input and output pattern:
+
+    - The input is a dataset struct (with at least ``.sa.chunks`` and ``.sa.targets`` properly set)
+    - The output consists of one or more values, which can be stored into a new output dataset
+    - Optionally, there may be some miscellaneous options that specify specifics on the operations.
+
+        + for split-half correlations, these could be how to normalize the correlations (the default is a Fisher-transform using the ``atanh`` function)
+        + for cross-validation, these are the partitioning scheme for cross-validation (e.g. odd-even or n-fold) and the classifier (e.g. LDA, SVM, or Naive Bayes) to use.
+
+This pattern is captured by the CoSMoMVPA dataset *measure* concept. An important reason for using measures is that it allows for a flexible implementation of *searchlights*, which involve the repeated application of the same measure to subsets of features.
+
+The measure concept
++++++++++++++++++++
+A dataset measure is a function with the following signature:
+
+    .. code-block:: matlab
+
+        output = dataset_measure(dataset, args)
+
+where:
+
+    - ``dataset`` is a dataset struct.
+    - ``args`` are options specific for that measure.
+    - ``output`` must be a ``struct`` with fields ``.samples`` (in column vector format) and optionally a field ``.sa``
+
+        + it should not have fields ``.fa``.
+        + usually it has no field ``.a`` (except for some complicated cases where it can have an ``.a.sdim`` field, if the measure returns data in a dimensional format).
+
+Split-half correlations using a measure
++++++++++++++++++++++++++++++++++++++++
+As a first exercise, load two datasets using subject ``s01``'s T-statistics for odd and even runs
+(``glm_T_stats_even.nii`` and ``glm_T_stats_odd.nii``) and the VT mask.
+Assign targets and chunks, then join the two datasets using ``cosmo_stack``. Then use :ref:`cosmo_correlation_measure` to:
+
+    - compute the correlation information (Fisher-transformed average of no-diagonal versus off-diagonal elements).
+    - the raw correlation matrix.
+
+Then compute the correlation information for each subject, and perform a t-test against zero over subjects.
+
+Template: :ref:`run_correlation_measure_skl`
+
+Check your answers here: :ref:`run_correlation_measure_skl` / :pb:`correlation_measure_skl`
+
+
+Classifier with cross-validation using a measure
+++++++++++++++++++++++++++++++++++++++++++++++++
+As a second exercise, load a dataset using subject ``s01``'s T-statistics for every run
+('glm_T_stats_perrun.nii') and the VT mask.
+
+Assign targets and chunks, then use the LDA classifier (:ref:`cosmo_classify_lda`) and n-fold partitioning (:ref:`cosmo_nfold_partitioner`) to perform classification accuracy using n-fold cross-validation.
+
+
+
+
+
+
+
+
---
+++ examples/run_correlation_measure.m
@@ -0,0 +1,159 @@
+%% Exercise with cosmo_correlation_measure
+
+%% Single subject analysis over all categories in VT
+subject_id='s01';
+mask_label='vt_mask';
+data_path=fullfile(study_path,subject_id); % data from subject s01
+
+% Define data locations and load data from even and odd runs
+mask_fn=fullfile(data_path, [mask_label '.nii']); % whole brain
+
+% Load data for odd runs
+data_odd_fn=fullfile(data_path,'glm_T_stats_odd.nii');
+ds_odd=cosmo_fmri_dataset(data_odd_fn,'mask',mask_fn,...
+                            'targets',1:6,'chunks',1);
+
+% Load data for even runs
+data_even_fn=fullfile(data_path,'glm_T_stats_even.nii');
+ds_even=cosmo_fmri_dataset(data_even_fn,'mask',mask_fn,...
+                            'targets',1:6,'chunks',2);
+
+% Combine even and odd runs into single dataset, store in ds_odd_even
+% Hint: use cosmo_stack
+% >@@>
+ds_odd_even=cosmo_stack({ds_odd, ds_even});
+% <@@<
+
+% remove constant features
+ds_odd_even=cosmo_remove_useless_data(ds_odd_even);
+
+% print dataset
+fprintf('Dataset input:\n');
+cosmo_disp(ds_odd_even);
+
+% Part 1: compute correlation difference
+% Hint: - apply cosmo_correlation_measure to ds_odd_even
+%       - because correlation difference and split-half correlations
+%         are the defaults, no optional arguments are required
+% >@@>
+ds_corr=cosmo_correlation_measure(ds_odd_even);
+% <@@<
+fprintf('\nDataset output (correlation difference):\n');
+cosmo_disp(ds_corr);
+
+fprintf(['Average correlation difference between matching and '...
+            'non-matching categories in %s for %s is %.3f\n'],...
+            mask_label, subject_id, ds_corr.samples);
+
+        
+% Part 2: compute the raw Fisher-transformed correlation matrix
+% Hint: use a struct 'args' with args.output='correlation' as second
+% argument for cosmo_correlation_measure
+% >@@>
+args=struct();
+args.output='correlation';
+c_raw=cosmo_correlation_measure(ds_odd_even,args);
+% <@@<
+fprintf('\nDataset output (Fisher-transformed correlations):\n');
+cosmo_disp(c_raw)
+
+% The data can be put in matrix form using cosmo_unflatten
+matrices=cosmo_unflatten(c_raw,1);
+imagesc(matrices);
+colorbar();
+
+%% Group analysis over all categories in VT
+
+% Compute correlation measure for each subject individually.
+subject_ids={'s01','s02','s03','s04','s05','s06','s07','s08'};
+nsubjects=numel(subject_ids);
+
+mask_label='vt_mask';
+
+ds_corrs=cell(nsubjects,1); % allocate space for output
+
+% Apply correlation measure for each subject using
+% cosmo_correlation_measure.
+% - compute the correlation measure using cosmo_correlation_measure;
+%   this should give a dataset structure with ds.samples of size 1x1
+% - because statistics are computed later, add to the output:
+%   * .sa.chunks : the subject number
+%   * .sa.targets: 1 (for each subject)
+% - store the result of the k-th subject in ds_corrs{k}
+% 
+for subject_num=1:nsubjects
+    subject_id=subject_ids{subject_num};
+    
+    % apply correlation measure for this subject, and store
+    % the result in 'ds_corr'
+    % >@@>
+    
+    % set path for this subject
+    data_path=fullfile(study_path,subject_id);
+
+    % Define data locations and load data from even and odd runs
+    mask_fn=fullfile(data_path, [mask_label '.nii']); % whole brain
+
+    data_odd_fn=fullfile(data_path,'glm_T_stats_odd.nii');
+    ds_odd=cosmo_fmri_dataset(data_odd_fn,'mask',mask_fn,...
+                                'targets',1:6,'chunks',1);
+
+
+    data_even_fn=fullfile(data_path,'glm_T_stats_even.nii');
+    ds_even=cosmo_fmri_dataset(data_even_fn,'mask',mask_fn,...
+                                'targets',1:6,'chunks',2);
+
+    % Combine even and odd runs
+    ds_odd_even=cosmo_stack({ds_odd, ds_even});
+
+    % remove constant features
+    ds_odd_even=cosmo_remove_useless_data(ds_odd_even);
+
+    % apply cosmo_correlation_measure
+    ds_corr=cosmo_correlation_measure(ds_odd_even);
+
+    % <@@<
+    % set targets and chunks for the output, so that cosmo_stat can be used
+    % below
+    ds_corr.sa.targets=1;
+    ds_corr.sa.chunks=subject_num;
+
+    ds_corrs{subject_num}=ds_corr;
+end
+
+% combine the data from all subjects
+ds_all=cosmo_stack(ds_corrs);
+
+%% Compute group-level statistics
+
+% run one-sample t-test again zero to compute the t-value and p-values
+% against the null hypothesis of a mean of zero.
+% 
+% hint: use matlab stats toolbox's ttest (if present), or
+%       use cosmo_stat:
+%       * for t-statistics, use 't' as second argument
+%       * to convert a 't' to 'p' value, use 'p' as third argument
+% >@@>
+ds_t=cosmo_stat(ds_all,'t');     % t-test against zero
+ds_p=cosmo_stat(ds_all,'t','p'); % convert to p-value
+
+samples=ds_all.samples;
+
+fprintf(['correlation difference in %s at group level: '...
+           '%.3f +/- %.3f, %s=%.3f, p=%.5f (using cosmo_stat)\n'],...
+            mask_label,mean(samples),std(samples),...
+            ds_t.sa.stats{1},ds_t.samples,ds_p.samples);
+% <@@<        
+
+% Using matlab's stat toolbox (if present)
+if cosmo_check_external('@stats',false)
+    % >@@>
+    [h,p,ci,stats]=ttest(samples);
+    fprintf(['correlation difference in %s at group level: '...
+            '%.3f +/- %.3f, t_%d=%.3f, p=%.5f (using matlab stats '...
+            'toolbox)\n'],...
+            mask_label,mean(samples),std(samples),stats.df,stats.tstat,p);
+    % <@@<
+else
+    fprintf('Matlab stats toolbox not found\n');
+end
