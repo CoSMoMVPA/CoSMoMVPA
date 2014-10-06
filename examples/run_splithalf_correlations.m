d4ff8b951b3d4d2aeb68a774fead65b0dd3c970b
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-10-06 22:39:50 +0200

    EXC+SML+RF: preallocate space for output

--- examples/run_splithalf_correlations.m
+++ examples/run_splithalf_correlations.m
@@ -12,14 +12,20 @@ labels = {'monkey'; 'lemur'; 'mallard'; 'warbler'; 'ladybug'; 'lunamoth'};
 nsubjects=numel(subject_ids);
 
 % allocate space for output
-mean_weighted_zs=zeros(nsubjects,1);
+sum_weighted_zs=zeros(nsubjects,1);
 
 config=cosmo_config();
 study_path=fullfile(config.tutorial_data_path,'ak6');
 
 
 %% Loop over rois
-for iRoi = 1:numel(rois)
+nrois=numel(rois);
+for iRoi = 1:nrois
+
+    % pre-allocate space weighted correlation difference
+    % in each subject
+    sum_weighted_zs=zeros(nsubjects,1);
+
     %% Computations for each subject
     for j=1:nsubjects
         subject_id=subject_ids{j};
@@ -48,11 +54,21 @@ for iRoi = 1:numel(rois)
         % Hint: use cosmo_corr
         % >@@>
         rho=cosmo_corr(half1_samples',half2_samples');
-        if j == 1
-            rho_sum(:, :, iRoi)=rho;
-        else
-            rho_sum(:, :, iRoi)=rho_sum(:, :, iRoi)+rho;%for the advanced exercise: sum up all individual correlation matrices
+        % <@@<
+
+        % for the advanced exercise: sum up all individual
+        % correlation matrices
+        % >@@>
+        if j == 1 && iRoi == 1
+            % first correlation was just computed, and we now know
+            % the number of conditions through the size of rho.
+            %
+            % allocate space for sum over subjects
+            nclasses=size(rho,1);
+            rho_sum=zeros([nclasses,nclasses,nrois]);
         end
+        rho_sum(:, :, iRoi)=rho_sum(:, :, iRoi)+rho;
+
         % <@@<
 
         % To make these correlations more 'normal', apply a Fisher
@@ -73,7 +89,13 @@ for iRoi = 1:numel(rois)
         % are going to be weighted.
         % The matrix must have a mean of zero, positive values on diagonal,
         % negative elsewhere.
-        contrast_matrix=eye(6)-1/6;
+
+        nclasses=size(rho,1);
+        % sanity check; in this example there are 6 conditions
+        assert(nclasses==6);
+
+        % set contrast matrix
+        contrast_matrix=(eye(nclasses)-1/nclasses)/nclasses;
 
         if abs(mean(contrast_matrix(:)))>1e-14
             error('illegal contrast matrix');
@@ -85,55 +107,65 @@ for iRoi = 1:numel(rois)
         % >@@>
         weighted_z=z.*contrast_matrix;
 
-        mean_weighted_z=mean(weighted_z(:));
+        sum_weighted_z=sum(weighted_z(:));
         % <@@<
 
-        % store the result for this subject
-        mean_weighted_zs(j)=mean_weighted_z;
+        % store the result for this subject in sum_weighted_zs, so that
+        % group statistics can be computed
+        % >@@>
+        sum_weighted_zs(j)=sum_weighted_z;
+        % <@@<
     end
 
     %% compute t statistic and print the result
     % run one-sample t-test again zero
 
-    % Using cosmo_stats - convert to dataset struct.
-    % The targets are chunks are set to indicate that all samples are from the
-    % same class (condition), and each observation is independent from the
-    % others
-    mean_weighted_zs_ds=struct();
-    mean_weighted_zs_ds.samples=mean_weighted_zs;
-    mean_weighted_zs_ds.sa.targets=ones(nsubjects,1);
-    mean_weighted_zs_ds.sa.chunks=(1:nsubjects)';
-
-
-    ds_t=cosmo_stat(mean_weighted_zs_ds,'t');     % t-test against zero
-    ds_p=cosmo_stat(mean_weighted_zs_ds,'t','p'); % convert to p-value
-
-    fprintf(['correlation difference in %s at group level: '...
-        '%.3f +/- %.3f, %s=%.3f, p=%.5f (using cosmo_stat)\n'],...
-        rois{iRoi},mean(mean_weighted_zs),std(mean_weighted_zs),...
-        ds_t.sa.stats{1},ds_t.samples,ds_p.samples);
-
     % Using matlab's stat toolbox (if present)
     if cosmo_check_external('@stats',false)
-        [h,p,ci,stats]=ttest(mean_weighted_zs);
-        fprintf(['Correlation difference in %s at group level: '...
+        [h,p,ci,stats]=ttest(sum_weighted_zs);
+        fprintf(['correlation difference in %s at group level: '...
             '%.3f +/- %.3f, t_%d=%.3f, p=%.5f (using matlab stats '...
             'toolbox)\n'],...
-            rois{iRoi},mean(mean_weighted_zs),std(mean_weighted_zs),...
+            rois{iRoi},mean(sum_weighted_zs),std(sum_weighted_zs),...
             stats.df,stats.tstat,p);
     else
         fprintf('Matlab stats toolbox not available\n');
     end
+
+    % Using cosmo_stats - convert to dataset struct.
+    % The targets are chunks are set to indicate that all samples are from
+    % the same class (condition), and each observation is independent from
+    % the others
+    sum_weighted_zs_ds=struct();
+    sum_weighted_zs_ds.samples=sum_weighted_zs;
+    sum_weighted_zs_ds.sa.targets=ones(nsubjects,1);
+    sum_weighted_zs_ds.sa.chunks=(1:nsubjects)';
+
+    ds_t=cosmo_stat(sum_weighted_zs_ds,'t');     % t-test against zero
+    ds_p=cosmo_stat(sum_weighted_zs_ds,'t','p'); % convert to p-value
+
+    fprintf(['correlation difference in %s at group level: '...
+        '%.3f +/- %.3f, %s=%.3f, p=%.5f (using cosmo_stat)\n'],...
+        rois{iRoi},mean(sum_weighted_zs),std(sum_weighted_zs),...
+        ds_t.sa.stats{1},ds_t.samples,ds_p.samples);
 
 end
 
 %advanced exercise: plot an image of the correlation matrix averaged over
 %participants (one for each roi)
 
+% allocate space for axis handles, so that later all plots can be set to
+% have the same color limits
+ax_handles=zeros(nrois,1);
+col_limits=zeros(nrois,2);
 
-for iRoi = 1:numel(rois)
+for iRoi = 1:nrois
     figure
-    axh(iRoi) = gca;
+
+    % store axis handle for current figure
+    ax_handles(iRoi) = gca;
+
+    % visualuize matrix
     % >@@>
 
     imagesc(rho_sum(:, :, iRoi)/nsubjects)
@@ -141,11 +173,13 @@ for iRoi = 1:numel(rois)
     set(gca, 'ytick', 1:numel(labels), 'yticklabel', labels)
 
     colorbar
-    title(sprintf('Average splithalf correlation across subjects in mask ''%s''', rois{iRoi}))
+    desc=sprintf(['Average splithalf correlation across subjects '...
+                    'in mask ''%s'''], rois{iRoi});
+    title(desc)
     % <@@<
 
-    colorLims(:, :, iRoi) = get(gca, 'clim');
+    col_limits(iRoi,:) = get(gca, 'clim');
 end
 %give all figures the same color limits such that correlations can be
 %compared visually
-set(axh, 'clim', [min(colorLims(:)), max(colorLims(:))])
+set(ax_handles, 'clim', [min(col_limits(:)), max(col_limits(:))])
