a2fbd9e5a139db3699e78ad4c705e515460699bc
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-10-07 17:24:21 +0200

    DOC+EXC: more explanations

--- examples/run_nfold_crossvalidate.m
+++ examples/run_nfold_crossvalidate.m
@@ -20,37 +20,85 @@ ds.sa.chunks = floor(((1:60)-1)/6)'+1;
 classes = {'monkey','lemur','mallard','warbler','ladybug','lunamoth'};
 ds.sa.labels = repmat(classes,1,10)';
 
+% this is good practice after setting attributes manually
+cosmo_check_dataset(ds);
+
 %% Part 1: 'manual' crossvalidation
 
-nsamples=size(ds.samples,1);
+nsamples=size(ds.samples,1); % should be 60 for this dataset
 
-% allocate space for output
+% allocate space for preditions for all 60 samples
 all_pred=zeros(nsamples,1);
 
-% get unique chunks, and ensure that fold_ids in the range 1..nfolds
-% (in this case they already are, but in other datasets they may not)
-[unq_folds,unused,fold_ids]=unique(ds.sa.chunks);
-nfolds=numel(unq_folds);
+% safety check:
+% to simplify this exercise, the code below assumes that .sa.chunks is in
+% the range 1..10; if that is not the case, the code may not work properly.
+% Therefore an 'assert' statement is used to verify that the chunks are as
+% required for the remainder of this exercise.
+assert(isequal(ds.sa.chunks,floor(((1:60)-1)/6)'+1));
 
+nfolds=numel(unq_folds); % should be 10
+
+% run n-fold cross-validation
+% in the k-th fold (k ranges from 1 to 10), test the LDA classifier on
+% samples with chunks==k and after training on all other samples.
+%
+% (in this exercise this is done manually, but easier solutions involve
+% using cosmo_nfold_partitioner and cosmo_crossvalidation_measure)
 for fold=1:nfolds
-    % in each fold:
-    % - slice the dataset twice, once to get a test dataset (which has
-    %   samples where .sa.chunks match fold_ids) and once to get a
-    %   training dataset (which has all other samples)
-    % - use cosmo_classify_lda to get predictions
-    % - store the results in all_pred at the locations index by test_msk
-    test_msk=fold_ids==fold;
-    ds_test=cosmo_slice(ds,test_msk);
-
+    % make a logical mask (of size 60x1) for the test set. It should have
+    % the value true where ds.sa.chunks has the same value as 'fold', and
+    % the value false everywhere else. Assign this to the variable
+    % 'test_msk'
     % >@@>
-    train_msk=~test_msk;
-    ds_train=cosmo_slice(ds,train_msk);
+    test_msk=ds.sa.chunks==fold;
+    % <@@<
 
-    pred=cosmo_classify_lda(ds_train.samples,ds_train.sa.targets,...
+    % slice the input dataset 'ds' across samples using 'test_msk' so that
+    % it has only samples in the 'fold'-th chunk. Assign the result to the
+    % variable 'ds_test';
+    % >@@>
+    ds_test=cosmo_slice(ds,test_msk);
+    % <@@<
+
+    % now make another logical mask (of size 60x1) for the training set.
+    % the value true where ds.sa.chunks has a different value as 'fold',
+    % and the value false everywhere else. Assign this to the variable
+    % 'train_msk'
+    % >@@>
+    train_msk=ds.sa.chunks~=fold;
+    % (alternative: train_msk=~test_msk)
+    % <@@<
+
+    % slice the input dataset again using train_msk, and assign to the
+    % variable 'ds_train'
+    % >@@>
+    ds_train=cosmo_slice(ds,train_msk);
+    % <@@<
+
+    % Use cosmo_classify_lda to get predicted targets for the
+    % samples in 'ds_test'. To do so, use the samples and targets
+    % from 'ds_train' for training (as first and second argument for
+    % cosmo_classify_lda), and the samples from 'ds_test' for testing
+    % (third argument for cosmo_classify_lda).
+    % Assign the result to the variable 'fold_pred', which should be a 6x1
+    % vector.
+    % >@@>
+    fold_pred=cosmo_classify_lda(ds_train.samples,ds_train.sa.targets,...
                                     ds_test.samples);
-    all_pred(test_msk)=pred;
+    % <@@<
+
+    % store the predictions from 'fold_pred' in the 'all_pred' vector,
+    % at the positions masked by 'test_msk'.
+    % >@@>
+    all_pred(test_msk)=fold_pred;
     % <@@<
 end
+
+% safety check:
+% for this exerice, the following code tests whether the predicted classes
+% is as they should be (i.e. the correct answer); if not an error is
+% raised.
 
 expected_pred=[ 1 1 1 2 1 1 2 1 1 1
                 2 1 2 2 2 2 2 2 2 2
@@ -59,26 +107,29 @@ expected_pred=[ 1 1 1 2 1 1 2 1 1 1
                 5 5 5 5 5 6 5 5 5 5
                 6 6 6 6 6 6 6 6 6 6];
 
-
-% the following code tests the code above (because it should give
-% identical output)
-
 % check that the output is as expected
 if ~isequal(expected_pred(:),all_pred)
     error('expected predictions to be row-vector with [%s]''',...
             sprintf('%d ',all_pred_alt));
 end
 
-%% Compute accuracy
+% Compute classification accuracy of all_pred compared to the targets in
+% the input dataset 'ds', and assign to a variable 'accuracy'
+% >@@>
 accuracy=mean(all_pred==ds.sa.targets);
+% <@@<
+
+% print the accuracy
 fprintf('\nLDA all categories n-fold: accuracy %.3f\n', accuracy);
 
-%% Show confusion matrix
+% Visualize confusion matrix
+% the cosmo_confusion_matrix convenience function is used to compute the
+% confusion matrix
+[confusion_matrix,classes]=cosmo_confusion_matrix(ds.sa.targets,all_pred);
 
-% hint: use cosmo_confusion_matrix
-% >@@>
-confusion_matrix=cosmo_confusion_matrix(ds.sa.targets,all_pred);
-% <@@<
+% print confusion matrix to terminal window
+fprintf('\nLDA n-fold cross-validation confusion matrix:\n')
+disp(confusion_matrix);
 
 % make a pretty figure
 figure
@@ -92,44 +143,75 @@ colorbar
 
 %% Part 2: use cosmo_nfold_partitioner
 
-% Make nfold partitioning scheme, and assign to the variable 'partitions'
-% Hint: use cosmo_nfold_partitioner
-% >@@>
+% This exercise replicates the analysis done in Part 1, but now
+% the the 'cosmo_nfold_partitioner' function is used to create a struct
+% that defines the cross-validation scheme (it contains the indices
+% for the train and set samples in each fold)
 partitions=cosmo_nfold_partitioner(ds);
-% <@@<
 
 % Show partitions
-fprintf('\nPartitions\n:');
+fprintf('\nPartitions for n-fold cross-validation:\n');
 cosmo_disp(partitions)
 
-% Count how many folds there are, and assign to the variable 'nfolds'
+% Count how many folds there are in 'partitions', and assign to the
+% variable 'nfolds'
 % >@@>
-nfolds=numel(partitions.train_indices);
+nfolds=numel(partitions.train_indices); % should be 10
 % <@@<
 
-% allocate space for output
+% allocate space for predictions of each sample (pattern) in 'ds'
 all_pred=zeros(nsamples,1);
 
+% As in part 1 (above), perform n-fold cross-validation using the LDA
+% classifier
 for fold=1:nfolds
     % implement cross-validation and store predicted labels in 'all_pred';
     % use the contents of partitions to slice the dataset in train and test
     % sets for each fold
+
+    % from the 'partitions' struct, get the train indices for the
+    % 'fold'-th fold and assign to a variable 'train_idxs'
     % >@@>
     train_idxs=partitions.train_indices{fold};
-    test_idxs=partitions.test_indices{fold};
+    % <@@<
 
+    % do the same for the test indices, and assign to a variable
+    % 'test_idxs'
+    % >@@>
+    test_idxs=partitions.test_indices{fold};
+    % <@@<
+
+    % slice the dataset twice:
+    % - once using 'train_idxs'; assign the result to 'ds_train'
+    % - once using 'test_idxs' ; assign the result to 'ds_test'
+    % >@@>
     ds_train=cosmo_slice(ds,train_idxs);
     ds_test=cosmo_slice(ds,test_idxs);
+    % <@@<
 
-    pred=cosmo_classify_lda(ds_train.samples,ds_train.sa.targets,...
+    % compute predictions for the samples in 'ds_test' after training
+    % using the samples and targets in 'ds_train'
+    % >@@>
+    fold_pred=cosmo_classify_lda(ds_train.samples,ds_train.sa.targets,...
                                     ds_test.samples);
-    all_pred(test_idxs)=pred;
+    % <@@<
+
+     % store the predictions from 'fold_pred' in the 'all_pred' vector,
+    % at the positions indexed by 'test_idxs'.
+    % >@@>
+    all_pred(test_idxs)=fold_pred;
     % <@@<
 end
 
+% Compute classification accuracy of all_pred compared to the targets in
+% the input dataset 'ds', and assign to a variable 'accuracy'
+% >@@>
+accuracy=mean(all_pred==ds.sa.targets);
+% <@@<
 accuracy=mean(all_pred==ds.sa.targets);
 fprintf(['\nLDA all categories n-fold (with partitioner): '...
             'accuracy %.3f\n'], accuracy);
 
 % Note: cosmo_crossvalidation_measure can perform the above operations as
-% well, but using that function is not part of this exercise
+% well (and in an easier way), but using that function is not part of
+% this exercise
