d0bd1ba42b9cd3847b0f7cdbc3b9be2e9b008663
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-10-27 11:00:09 +0100

    NF+RF: support -dry argument, and some refactorings to simplify the code

--- mvpa/cosmo_publish_run_scripts.m
+++ mvpa/cosmo_publish_run_scripts.m
@@ -4,27 +4,32 @@ function cosmo_publish_run_scripts(varargin)
 % cosmo_publish_build_html([force|fn])
 %
 % Inputs:
-%   force        boolean; indicates whether to always rebuild the output
-%                even if it already exists. If false (the default), then
-%                only run_* scripts that are newer than the output are
-%                published again. If true, all run_* scripts are published
-%                (rebuilt)
-%   fn           filename of matlab file to build
+%   fn           filename of matlab file to publish (in the examples
+%                directory)
+%   '-force'     force rebuild, even if an output file is newer than the
+%                corresponding output file
+%   '-dry'       dry run: show which files would be published
 %
 % Notes:
+%  - if no filename is given, then all files are rebuilt if necessary
 %  - this function is intended for developers (to build the website)
-%  - it requires
+%  - whether an output file is out of date is first determined using
+%    git: if git shows no changes to the last commit of the input file
+%    then it is assumed it is not out of date. If the input file has
+%    been changed since the last commit (or never been comitted), then
+%    the modification date is used to determine whether it is out of date.
+%  - requirements
 %    * a Unix-like system
 %    * a working installation of git
 %    * CoSMoMVPA code present in a git repository
 %
 % NNO Sep 2014
 
-    [force, srcpat]=process_input(varargin{:});
+    [force, srcpat, dryrun]=process_input(varargin{:});
 
     % save original working directory
     pdir=pwd();
-    c=onCleanup(@()cd(pdir));
+    cleaner1=onCleanup(@()cd(pdir));
 
     medir=fileparts(which(mfilename()));
     cd(medir);
@@ -37,21 +42,15 @@ function cosmo_publish_run_scripts(varargin)
 
     summaryfn='index.html';
 
-    if nargin<1
-        force=false;
-    end
-
     if ~exist(trgdir,'file');
         mkdir(trgdir);
     end
 
     srcfns=dir(fullfile(srcdir,[srcpat srcext]));
-
-    if isempty(srcfns)
+    nsrc=numel(srcfns);
+    if nsrc==0
         error('No files found matching %s%s in %s',[srcpat srcext],srcdir);
     end
-
-    nsrc=numel(srcfns);
 
     outputs=cell(nsrc,1);
 
@@ -59,64 +58,72 @@ function cosmo_publish_run_scripts(varargin)
     has_pwd=~isempty(strfind(p,medir));
     if ~has_pwd
         addpath(medir)
+        cleaner2=onCleanup(@()rmpath(medir));
     end
+
+    total_time_took=0;
 
     output_pos=0;
     for k=1:nsrc
         cd(medir);
-        update=true;
         srcfn=fullfile(srcdir,srcfns(k).name);
         [srcpth,srcnm,unused]=fileparts(srcfn);
         trgfn=fullfile(trgdir,[srcnm trgext]);
-        if ~force && ~needs_update(srcfn,trgfn);
-            update=false;
-            fprintf('skipping %s%s\n',...
-                        srcnm,srcext);
-            output_pos=output_pos+1;
-            outputs{output_pos}=srcnm;
+
+        [needs_update,build_msg]=target_needs_update(srcfn,trgfn);
+        if force
+            build_msg=sprintf('update forced: %s%s',srcname,srcext);
         end
 
+        fprintf(build_msg);
 
-        if ~update
-            continue;
-        end
+        do_update=needs_update || force;
 
+        if do_update
+            fprintf('\n   building ... ');
+            cd(srcpth);
+            is_built=false;
+            clock_start=clock();
+            try
+                publish(srcnm, struct('outputDir',trgdir,'catchError',false));
+                is_built=true;
+            catch me
+                fnout=fullfile(trgdir,[srcnm trgext]);
+                if exist(fnout,'file')
+                    delete(fnout);
+                end
 
-        fprintf('building: %s ...', srcnm);
+                warning('Unable to build %s%s: %s', srcnm, srcext, me.message);
+                fprintf('%s\n', me.getReport);
+            end
+            clock_end=clock();
+            time_took=etime(clock_end,clock_start);
+            total_time_took=total_time_took+time_took;
 
-        cd(srcpth);
-        is_built=false;
-        try
-            publish(srcnm, struct('outputDir',trgdir,'catchError',false));
-            is_built=true;
-        catch me
-            fnout=fullfile(trgdir,[srcnm trgext]);
-            if exist(fnout,'file')
-                delete(fnout);
+            cd(medir);
+
+            if is_built
+                outcome_msg=' done';
+            else
+                outcome_msg=' !! failed';
             end
 
-            warning('Unable to build %s%s: %s', srcnm, srcext, me.message);
-            fprintf('%s\n', me.getReport);
+            status_msg=sprintf('%s (%.1f sec)', outcome_msg,time_took);
+        else
+            status_msg='';
         end
-        cd(medir);
 
-        if ~is_built
-            fprintf(' !! building failed\n');
-            continue
-        end
-        fprintf(' done\n');
+        fprintf('%s\n', status_msg);
 
         output_pos=output_pos+1;
         outputs{output_pos}=srcnm;
     end
 
-    if ~has_pwd
-        rmpath(medir);
-    end
+    fprintf('Processed %d files (%.1f sec)\n',output_pos,total_time_took);
 
     outputfn=fullfile(trgdir, summaryfn);
     fid=fopen(outputfn,'w');
-    c_=onCleanup(@()fclose(fid));
+    cleaner3=onCleanup(@()fclose(fid));
     fprintf(fid,['<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"'...
             '>\n']);
     fprintf(fid,['<HTML><HEAD><TITLE>Index of matlab outputs</TITLE>'...
@@ -132,9 +139,10 @@ function cosmo_publish_run_scripts(varargin)
 
 
 
-function [force, srcpat]=process_input(varargin)
+function [force, srcpat, dryrun]=process_input(varargin)
     force=false;
     srcpat=[];
+    dryrun=false;
     n=numel(varargin);
 
     for k=1:n
@@ -142,6 +150,8 @@ function [force, srcpat]=process_input(varargin)
         if ischar(arg)
             if strcmp(arg,'-force')
                 force=true;
+            elseif strcmp(arg,'-dry')
+                dryrun=true;
             elseif ~isempty(srcpat)
                 error('multiple inputs found, this is not supported');
             else
@@ -157,28 +167,35 @@ function [force, srcpat]=process_input(varargin)
         srcpat='*_*';
     end
 
-function tf=needs_update(srcfn,trgfn)
+function [tf,msg]=target_needs_update(srcfn,trgfn)
     % helper function to see if html is out of date
-    [unused,root,unused]=fileparts(srcfn);
-    [unused,root_alt,unused]=fileparts(trgfn);
+    [unused,root,srcext]=fileparts(srcfn);
+    [unused,root_alt,trgext]=fileparts(trgfn);
     assert(isequal(root,root_alt));
 
+    srcname=[root,srcext];
+    trgname=[root,trgext];
     t_trg=time_last_changed(trgfn);
     if isnan(t_trg)
         % does not exist, so needs update
         tf=true;
+        msg=sprintf('not found: %s', trgname);
         return;
     end
 
-    t_src=time_last_commit(srcfn);
-    if isnan(t_src) || is_in_staging(srcfn) || is_untracked(srcfn)
-        % does not exist, or in staging, or untracked
+    if is_in_staging(srcfn) || is_untracked(srcfn)
         t_src=time_last_changed(srcfn);
+        tf=isnan(t_src) || t_trg<t_src;
+        msg=sprintf('modified: %s', srcname);
+    else
+        t_src=time_last_commit(srcfn);
+        tf=isnan(t_src) || t_trg<t_src;
+        msg=sprintf('recent commit: %s',srcname);
     end
 
-    % needs update if source file date unknown, or newer than html
-    tf=isnan(t_src) || t_trg<t_src;
-
+    if ~tf
+        msg=sprintf('up to date: %s',trgname);
+    end
 
 function t=time_last_changed(fn)
     d=dir(fn);
