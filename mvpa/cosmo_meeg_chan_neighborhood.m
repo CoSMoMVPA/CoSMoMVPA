21efd3221f0ad6bfe6513c05a31804c6c2283e10
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-12-17 18:41:38 +0100

    NF: MEEG channel neighborhood

---
+++ mvpa/cosmo_meeg_chan_neighborhood.m
@@ -0,0 +1,104 @@
+function nbrhood=cosmo_meeg_chan_neighborhood(ds, varargin)
+    [unused,unused,attr_name,dim_name,ds_label]=cosmo_dim_find(ds,...
+                                                        'chan',true);
+    nbrs=get_neighbors(ds,varargin{:});
+
+
+    ds_chan=ds.(attr_name).chan;
+    [nbrs_label,chan_idxs]=get_neighbor_indices(nbrs,ds_label,ds_chan);
+
+    nbrhood=struct();
+    nbrhood.neighbors=chan_idxs;
+    nbrhood.(attr_name).chan=numel(nbrs);
+    nbrhood.a.(dim_name).labels={'chan'};
+    nbrhood.a.(dim_name).values={nbrs_label};
+
+function [nbrs_label,chan_idxs]=get_neighbor_indices(nbrs,ds_label,ds_chan)
+    nbrs_label={nbrs.label};
+    ds_label_cell=cellfun(@(x){x},ds_label,'UniformOutput',false);
+
+    ncenter=numel(nbrs_label);
+
+    ow=cosmo_overlap({nbrs.neighblabel},ds_label_cell);
+    [chan_pos,chan_cell]=cosmo_index_unique({ds_chan});
+    chan=chan_cell{1};
+    %
+    chan_idxs=cell(ncenter,1);
+    for k=1:ncenter
+        % find the indices in ds where the neighbors match
+        idx=find(ow(k,:));
+        chan_pos_msk=cosmo_match(chan,idx);
+        chan_idx=cat(1,chan_pos{chan_pos_msk});
+        if isempty(chan_idx)
+            chan_idx=zeros(0,1);
+        end
+        chan_idxs{k}=chan_idx;
+    end
+
+
+function nbrs=get_neighbors(ds, varargin)
+    if numel(varargin)==1
+        nbrs=varargin{1};
+    else
+        nbrs=cosmo_meeg_chan_neighbors(ds,varargin{:});
+    end
+
+    verify_neighbors(nbrs)
+
+function verify_neighbors(nbrs)
+    if ~isstruct(nbrs) || ...
+                ~isfield(nbrs,'label') || ~isfield(nbrs,'neighblabel')
+        error('neighbor struct must be neighborhood struct');
+    end
+
+
+
+function foo
+
+
+return
+
+ov=cosmo_overlap({nbrs.neighblabel},x);
+
+keep_idxs=find(any(ov,2));
+
+nkeep=numel(keep_idxs);
+
+label=nbrs_label(keep_idxs);
+
+ynbr=cell(1,nkeep);
+for k=1:nkeep
+    ynbr{k}=find(ov(keep_idxs(k),:));
+end
+
+
+
+
+
+
+return
+
+
+nx=numel(x);
+ny=numel(y);
+
+ov=cosmo_overlap(y,x);
+
+[row,col]=find(ov);
+
+label=nbrs_label(row);
+
+if numel(row)~=nx
+    % if not all labels in ds, use all labels in neighborhood
+    row=col;
+end
+
+n=numel(label);
+
+ynbr=cell(n,1);
+
+for k=1:n
+    ynbr{k}=nbrs(row(k)).neighblabel;
+end
+
+y_ov=cosmo_overlap(ynbr,x);
---
+++ tests/test_meeg_chan_neighborhood.m
@@ -0,0 +1,67 @@
+function test_suite=test_meeg_chan_neighbors()
+    initTestSuite;
+
+function test_neighbors()
+
+    test_step=10;
+
+    ds=cosmo_synthetic_dataset('type','meeg','size','big');
+    msk=ds.fa.chan<10 | mod(ds.fa.chan,5)==0 | ds.fa.chan>250;
+    imsk=find(msk);
+    rp=randperm(numel(imsk));
+    ds=cosmo_dim_slice(ds,imsk(rp),2);
+    n=numel(ds.a.fdim.values{1});
+    ds.a.fdim.values{1}=ds.a.fdim.values{1}(randperm(n));
+
+    nbrs=cosmo_meeg_chan_neighbors(ds,'chantype','meg_planar','radius',0);
+    nh=cosmo_meeg_chan_neighborhood(ds,nbrs);
+
+    assertEqual(nh.a.fdim.values,{{nbrs.label}});
+    assertEqual(nh.a.fdim.labels,{'chan'});
+
+    n=numel(nh.neighbors);
+    for k=1:test_step:n
+        idx=nh.neighbors{k};
+        chan_label=ds.a.fdim.values{1}(ds.fa.chan(idx));
+        assert(all(cosmo_match(chan_label,nh.a.fdim.values{1}{k})));
+    end
+
+    % test correspondence with neighbors
+    args={'chantype','all','delaunay',true};
+    nbrs=cosmo_meeg_chan_neighbors(ds,args{:});
+    nh=cosmo_meeg_chan_neighborhood(ds,args{:});
+    nh2=cosmo_meeg_chan_neighborhood(ds,nbrs);
+    assertEqual(nh,nh2);
+
+    ds_label=ds.a.fdim.values{1};
+
+    n=numel(nh.neighbors);
+    for k=1:test_step:n
+        idx=nh.neighbors{k};
+        chan_label=ds.a.fdim.values{1}(ds.fa.chan(idx));
+        assertEqual(intersect(ds_label,nbrs(k).neighblabel),...
+                        unique(chan_label));
+    end
+
+
+    % try with dataset labels
+    args={'chantype','all','count',5,'label','dataset'};
+    nbrs=cosmo_meeg_chan_neighbors(ds,args{:});
+    nh=cosmo_meeg_chan_neighborhood(ds,nbrs);
+    n=numel(nh.neighbors);
+    assertEqual(n, numel(ds.a.fdim.values{1}));
+
+    ds_label=ds.a.fdim.values{1};
+    nh_label=nh.a.fdim.values{1};
+
+    for k=1:test_step:n
+        i=find(cosmo_match(nh_label,ds_label{k}));
+        assertEqual(nbrs(i).label,ds_label{k});
+
+        overlap=cosmo_overlap({ds_label(ds.fa.chan(nh.neighbors{i}))},...
+                                {nbrs(i).neighblabel});
+        assertEqual(overlap,1);
+    end
+
+
+
