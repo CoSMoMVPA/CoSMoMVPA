fb1e26bf2f6f2c7d376b27e1a1280bc70cc937a4
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-12-19 19:12:18 +0100

    ENH: update example to reflect new interface for timeseries aclassification

---
+++ examples/demo_meeg_timeseries_classification.m
@@ -0,0 +1,135 @@
+%% MEEG timeseries classification
+%
+% This example shows MVPA analyses performed on MEEG data.
+%
+% The input dataset involved a paradigm with electrical median nerve
+% stimulation for durations of 2s at 20Hz.
+%
+% The code presented here can be adapted for other MEEG analyses, but
+% there are a few potential caveats:
+%
+% * assignment of targets (labels of conditions) is based here on
+%   stimulation periods versus pre-stimulation periods. In typical
+%   analyses the targets should be based on different trial conditions, for
+%   example as set a FieldTrip .trialinfo field.
+% * assignment of chunks (parts of the data that are assumed to be
+%   independent) is done randomly. In typical analyses they can be based on
+%   different runs, with one chunk value per run.
+% * the time window used for analyses is rather small. This means that in
+%   particular for time-freq analysis a lot of data is missing, especially
+%   for early and late timepoints in the lower frequency bands. For typical
+%   analyses it may be preferred to use a wider time window.
+% * the current examples do not perform baseline corrections or signal
+%   normalizations, which may reduce discriminatory power.
+%
+% Note: running this code requires FieldTrip.
+%
+% NNO Jan 2014
+
+
+%% get timelock data in CoSMoMVPA format
+
+% set configuration
+config=cosmo_config();
+data_path=fullfile(config.tutorial_data_path,'meg_20hz');
+
+% show dataset information
+readme_fn=fullfile(data_path,'README');
+cosmo_type(readme_fn);
+
+% reset citation list
+cosmo_check_external('-tic');
+
+% load data
+data_fn=fullfile(data_path,'subj102_B01_20Hz_timelock.mat');
+data_tl=load(data_fn);
+
+% convert to cosmomvpa struct
+ds_tl=cosmo_meeg_dataset(data_tl);
+
+% set the target (trial condition)
+ds_tl.sa.targets=ds_tl.sa.trialinfo; % 1=pre, 2=post
+
+% in addition give a label to each trial
+index2label={'pre','post'}; % 1=pre, 2=peri/post
+ds_tl.sa.labels=cellfun(@(x)index2label(x),num2cell(ds_tl.sa.trialinfo));
+
+% just to check everything is ok
+cosmo_check_dataset(ds_tl);
+
+%% Prepare MVPA
+% reset chunks: use four chunks
+nchunks=4;
+ds_tl=cosmo_chunkize(ds_tl,nchunks);
+
+% do a take-one-fold out cross validation.
+% except when using a splithalf correlation measure it is important that
+% the partitions are *balanced*, i.e. each target (or class) is presented
+% equally often in each chunk
+partitions=cosmo_nchoosek_partitioner(ds_tl,1);
+partitions=cosmo_balance_partitions(partitions, ds_tl);
+
+npartitions=numel(partitions);
+fprintf('There are %d partitions\n', numel(partitions.train_indices));
+fprintf('# train samples:%s\n', sprintf(' %d', cellfun(@numel, ...
+                                        partitions.train_indices)));
+fprintf('# test samples:%s\n', sprintf(' %d', cellfun(@numel, ...
+                                        partitions.test_indices)));
+
+
+% define the measure and its argument.
+% here a simple naive baysian classifier is used.
+% Alternative are @cosmo_classify_{svm,nn,lda}.
+measure=@cosmo_crossvalidation_measure;
+measure_args.classifier=@cosmo_classify_naive_bayes;
+measure_args.partitions=partitions;
+
+%% Run time-series searchlight on magneto- and gradio-meters seperately
+% compute and plot accuracies for magnetometers and gradiometers separately
+% (because we collapse of channels (nbrhoof_nf(1)==0) no need for
+% planar2cmb
+chantypes={'meg_axial','meg_planar'};
+
+% in the time searchlight analysis, select the time-point itself, the two
+% timepoints after it, and the two timepoints before it
+time_radius=2;
+nchantypes=numel(chantypes);
+
+ds_chantypes=cosmo_meeg_chantype(ds_tl);
+
+for k=1:nchantypes
+    parent_type=chantypes{k};
+
+    % find feature indices of channels matching the parent_type
+    chantype_idxs=find(cosmo_match(ds_chantypes,parent_type));
+
+    % define mask with those features
+    chan_msk=cosmo_match(ds_tl.fa.chan,chantype_idxs);
+
+
+    ds_tl_sel=cosmo_dim_slice(ds_tl, chan_msk, time_radius);
+
+    % define neighborhood over time; for each time point only the time
+    % point itself is incldued
+    nbrhood=cosmo_interval_neighborhood(ds_tl_sel,'time',2);
+
+    % run the searchlight using the measure, measure arguments, and
+    % neighborhood defined above.
+    sl_map=cosmo_searchlight(ds_tl_sel,measure,'nbrhood',nbrhood,'args',...
+                                                        measure_args);
+    fprintf('The output has feature dimensions: %s\n', ...
+                    cosmo_strjoin(sl_map.a.fdim.labels,', '));
+
+    subplot(1,nchantypes,k);
+
+    time_values=sl_map.a.fdim.values{1}; % first dim (channels got nuked)
+    plot(time_values,sl_map.samples);
+    title(strrep(parent_type,'_',' '));
+    ylim([.4 .8])
+    xlim([min(time_values),max(time_values)]);
+    ylabel('classification accuracy (chance=.5)');
+    xlabel('time');
+end
+% Show citation information
+cosmo_check_external('-cite');
+
