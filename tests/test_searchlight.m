a5c50e4df893506ed786859d8826996e2fb89353
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-01-11 12:15:42 +0100

    TST: update unit test for searchlight to reflect new s syntax

--- tests/test_searchlight.m
+++ tests/test_searchlight.m
@@ -5,30 +5,36 @@ function test_suite = test_searchlight
 function test_searchlight_
 
     ds=generate_test_dataset();
+
     measure=@(x,a) cosmo_structjoin('samples',size(x.samples,2));
-    m=cosmo_searchlight(ds, measure,'radius',3,'progress',0);
+    nh=cosmo_spherical_neighborhood(ds,'radius',3,'progress',0);
+
+    m=cosmo_searchlight(ds, measure,'nbrhood',nh,'progress',false);
+
     assertVectorsAlmostEqual(histc(m.samples,[38 46 47 100 123]),...
                             [ 24 24 558 352 35])
 
+    nh2=cosmo_spherical_neighborhood(ds,'count',18,'progress',0);
+    m=cosmo_searchlight(ds, measure,'nbrhood',nh2,'progress',0);
+    assertVectorsAlmostEqual(histc(m.samples,16:19),...
+                            [24 76 406 495]);
 
-    m=cosmo_searchlight(ds, measure,'radius',-18,'progress',0);
-    assertVectorsAlmostEqual(histc(m.samples,[18 19 20 23 26]),...
-                            [398 495 8 24 76]);
-
-    d=cosmo_slice(ds,mod(ds.fa.i,3)==0,2);
-    m=cosmo_searchlight(d, measure,'radius',-18,'progress',0);
-    assertVectorsAlmostEqual(histc(m.samples,[18 19 21]),[80 56 150]);
-    assertVectorsAlmostEqual(m.samples([100 201]),[21 18]);
+    ds_small=cosmo_slice(ds,mod(ds.fa.i,3)==0,2);
+    nh3=cosmo_spherical_neighborhood(ds_small,'count',18,'progress',0);
+    m=cosmo_searchlight(ds_small, measure,'nbrhood',nh3,'progress',0);
+    assertVectorsAlmostEqual(histc(m.samples,16:19),[4 4 80 198]);
+    assertVectorsAlmostEqual(m.samples([100 201]),[19 19]);
 
 
 
     % bit of a smoke test
     measure=@cosmo_correlation_measure;
 
-    nbrhood=cosmo_spherical_neighborhood(ds,'radius',3,...
+    nh4=cosmo_spherical_neighborhood(ds,'radius',3,...
                                 cosmo_structjoin('progress',0));
-    m=cosmo_searchlight(ds, measure,'nbrhood',nbrhood,...
+    m=cosmo_searchlight(ds, measure,'nbrhood',nh4,...
                             'center_ids',[100 201],'progress',0);
 
     assertVectorsAlmostEqual(m.samples, [0.0075    0.1088]...
                                         ,'relative',.001);
+
