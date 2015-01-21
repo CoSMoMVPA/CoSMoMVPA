d69429803720c8aa6609ebec74213f16fd978c20
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-01-21 17:26:48 +0100

    TST+RF: put non-pearson correlation in separate test

--- tests/test_corr.m
+++ tests/test_corr.m
@@ -2,39 +2,51 @@ function test_suite = test_corr
     initTestSuite;
 
 
-function test_corr_
+function test_corr_no_type
     x=randn(10,20);
     y=randn(10,5);
 
     aeaa=@(x,y)assertElementsAlmostEqual(x,y);
-    aet=@(args,e)assertExceptionThrown(@()cosmo_corr(args{:}),e);
 
     aeaa(cosmo_corr(x),corr(x));
     aeaa(cosmo_corr(x,y),corr(x,y));
     aeaa(cosmo_corr(y,x),corr(y,x));
 
+function test_corr_with_type
     if cosmo_wtf('is_octave')
         cosmo_notify_test_skipped(['Non-Pearson correlation cannot '...
                         'be tested because Octave''s ''corr'' function '...
-                        'does not suport it']);
+                        'does not support it']);
+        return
+    end
 
+    x=randn(10,20);
+    y=randn(10,5);
+
+    aeaa=@(x,y)assertElementsAlmostEqual(x,y);
+    aeaa(cosmo_corr(x,'Pearson'),corr(x,'type','Pearson'));
+    aeaa(cosmo_corr(x,'Spearman'),corr(x,'type','Spearman'));
+
+
+    aeaa(cosmo_corr(x,'Pearson'),corr(x,'type','Pearson'));
+    aeaa(cosmo_corr(x,y,'Pearson'),corr(x,y,'type','Pearson'));
+
+function test_corr_exceptions
+    if cosmo_wtf('is_octave')
         id_minrhs='Octave:undefined-function';
         id_unknown_type='Octave:invalid-fun-call';
         id_innerdim='Octave:nonconformant-args';
     else
-        aeaa(cosmo_corr(x,'Pearson'),corr(x,'type','Pearson'));
-        aeaa(cosmo_corr(x,'Spearman'),corr(x,'type','Spearman'));
-
-
-        aeaa(cosmo_corr(x,'Pearson'),corr(x,'type','Pearson'));
-        aeaa(cosmo_corr(x,y,'Pearson'),corr(x,y,'type','Pearson'));
 
         id_minrhs='MATLAB:minrhs';
         id_unknown_type='stats:corr:UnknownType';
         id_innerdim='MATLAB:innerdim';
     end
 
+    aet=@(args,e)assertExceptionThrown(@()cosmo_corr(args{:}),e);
 
+    x=randn(10,20);
+    y=randn(10,5);
 
     aet({},id_minrhs);
     aet({x,'foo'},id_unknown_type);
