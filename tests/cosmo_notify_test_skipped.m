36e528cb00c41a904f87d0fd9c90f34517d7e07a
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-01-25 18:08:57 +0100

    NF: notification that test was skipped

---
+++ tests/cosmo_notify_test_skipped.m
@@ -0,0 +1,17 @@
+function cosmo_notify_test_skipped(reason)
+    db=dbstack('-completenames');
+
+    db_up=db(2);
+    desc=sprintf('%s: %s', db_up.name, reason);
+
+    if test_was_run_by_MOxUnit(db)
+        moxunit_throw_test_skipped_exception(desc);
+    else
+        warning('CoSMoMVPA:skipTest','Skipping test in %s (%s:%d)',...
+                desc, db_up.file, db_up.line);
+    end
+
+function tf=test_was_run_by_MOxUnit(db)
+    files={db.file};
+    tf=any(~cellfun(@isempty,regexp(files,'@MOxUnit')));
+
