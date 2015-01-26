506818287cf2d973695e1b20acd996659e7fcd0f
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-01-26 15:04:07 +0100

    ENH+NF+RF+TST+ACK: support partial correlations in target_dsm_corr_measure. Thanks to #Liuba Papeo# for suggesting this feature

--- mvpa/cosmo_target_dsm_corr_measure.m
+++ mvpa/cosmo_target_dsm_corr_measure.m
@@ -8,22 +8,30 @@ function ds_sa = cosmo_target_dsm_corr_measure(ds, varargin)
 %                  Q features
 %   args           struct with fields:
 %     .target_dsm  Either:
-%                  - target dissimilarity matrix of size PxP. It should have
-%                    zeros on the diagonal and be symmetric.
+%                  - target dissimilarity matrix of size PxP. It should
+%                    have zeros on the diagonal and be symmetric.
 %                  - target dissimilarity vector of size Nx1, with
 %                    N=P*(P-1)/2 the number of pairs of samples in ds.
-%     .metric      Distance metric used in pdist to compute pair-wise
-%                  distances between samples in ds. It accepts any
-%                  metric supported by pdist (default: 'correlation')
-%     .type        Type of correlation between target_dsm and ds,
-%                  one of 'Pearson' (default), 'Spearman', or 'Kendall'
+%     .metric      (optional) distance metric used in pdist to compute
+%                  pair-wise distances between samples in ds. It accepts
+%                  any metric supported by pdist (default: 'correlation')
+%     .type        (optional) type of correlation between target_dsm and
+%                  ds, one of 'Pearson' (default), 'Spearman', or
+%                  'Kendall'.
+%     .regress_dsm (optional) target dissimilarity matrix or vector (as
+%                  .target_dsm) that should be regressed out. If this
+%                  option is provided then the output is the partial
+%                  correlation between the pairwise distances between
+%                  samples in ds and target_dsm, after controlling for the
+%                  effect in regress_dsm.
 %
 %
 % Output:
-%    ds_sa           Dataset struct with fields:
-%      .samples      Scalar correlation value
-%      .sa           Struct with field:
-%        .labels     {'rho'}
+%    ds_sa         Dataset struct with fields:
+%      .samples    Scalar correlation value between the pair-wise
+%                  distances of the samples in ds and target_dsm.
+%      .sa         Struct with field:
+%        .labels   {'rho'}
 %
 % Examples:
 %     % generate synthetic dataset with 6 classes (conditions),
@@ -70,16 +78,98 @@ function ds_sa = cosmo_target_dsm_corr_measure(ds, varargin)
                             'metric','correlation',...
                             varargin);
 
-    if ~isfield(params,'target_dsm')
-        error('Missing parameter ''target_dsm''');
+    check_input(ds);
+
+    % - compute the pair-wise distance between all dataset samples using
+    %   cosmo_pdist
+
+    ds_pdist = cosmo_pdist(ds.samples, params.metric)';
+
+    % number of pairwise distances; should match that of target_dsm_vec
+    % below
+    npairs_dataset=numel(ds_pdist);
+
+    % get target dsm in vector form
+    target_dsm_vec=get_dsm_vec(params,'target_dsm',npairs_dataset);
+
+    % ensure the size of the dataset matches the matrix
+
+    has_regress_dsm=isfield(params,'regress_dsm');
+    if has_regress_dsm
+        regress_dsm_vec=get_dsm_vec(params,'regress_dsm',npairs_dataset);
+
+        % overwrite
+        [ds_pdist(:),target_dsm_vec(:)]=regress_out(ds_pdist,...
+                                                target_dsm_vec,...
+                                                regress_dsm_vec);
+    end
+
+
+    % >@@>
+    % compute correlations between 'pd' and 'target_dsm_vec', store in 'rho'
+    rho=cosmo_corr(ds_pdist,target_dsm_vec, params.type);
+    % <@@<
+
+    % store results
+    ds_sa=struct();
+    ds_sa.samples=rho;
+    ds_sa.sa.labels={'rho'};
+    ds_sa.sa.metric={params.metric};
+    ds_sa.sa.type={params.type};
+
+
+function [ds_resid,target_resid]=regress_out(ds_pdist,...
+                                                target_dsm_vec,...
+                                                regress_dsm_vec)
+    % set up design matrix
+    nsamples=size(ds_pdist,1);
+    regr=[regress_dsm_vec ones(nsamples,1)];
+
+    % put ds_pdist and target_dsm_vec together
+    both=[ds_pdist target_dsm_vec];
+
+    % compute residuals
+    both_resid=both-regr*(regr\both);
+
+    ds_resid=both_resid(:,1);
+    target_resid=both_resid(:,2);
+
+
+function dsm_vec=get_dsm_vec(params,name,npairs_dataset)
+    % helper funciton to get dsm in vector form
+    if ~isfield(params,name)
+        error('Missing parameter ''%s''',name);
+    end
+
+    dsm=params.(name);
+
+    if isrow(dsm)
+        dsm_vec=dsm';
+    elseif iscolumn(dsm)
+        dsm_vec=dsm;
+    else
+        % convert square matrix to vector
+        dsm_vec=cosmo_squareform(params.target_dsm,'tovector')';
+    end
+
+    if npairs_dataset ~= numel(dsm_vec),
+        error(['Sample size mismatch between dataset (%d pairs) '...
+                    'and ''%s'' in vector form (%d pairs)'], ...
+                        npairs_dataset,name,numel(dsm_vec));
+    end
+
+
+
+function check_input(ds)
+    % for safety require targets to be 1:N
+    has_sa=isstruct(ds) && isfield(ds,'sa');
+    if ~has_sa || ~isfield(ds.sa,'targets') || ~isfield(ds,'samples')
+        error('Missing field .sa.targets or .samples');
     end
 
     nsamples=size(ds.samples,1);
-    % for safety require targets to be 1:N
-    has_sa=isfield(ds,'sa');
-    if ~has_sa || ~isfield(ds.sa,'targets')
-        error('Missing field .sa.targets');
-    elseif ~isequal(ds.sa.targets',1:nsamples)
+
+    if ~isequal(ds.sa.targets',1:nsamples)
         msg=sprintf('.sa.targets must be (1:%d)''',nsamples);
         if isequal(unique(ds.sa.targets),(1:nsamples)');
             msg=sprintf(['%s\nMultiple samples with the same chunks '...
@@ -90,37 +180,3 @@ function ds_sa = cosmo_target_dsm_corr_measure(ds, varargin)
         end
         error(msg);
     end
-
-    % - compute the pair-wise distance between all dataset samples using
-    %   cosmo_pdist and store in 'pd'
-    % >@@>
-    pd = cosmo_pdist(ds.samples, params.metric);
-    % <@@<
-
-    target_dsm=params.target_dsm;
-
-    if isvector(target_dsm)
-        target_dsm_vec=target_dsm;
-    else
-        % convert square matrix to vector
-        target_dsm_vec=cosmo_squareform(params.target_dsm);
-    end
-        % target_dsm is a vector
-
-    if numel(pd) ~= numel(target_dsm_vec),
-        error(['Sample size mismatch between dataset (%d) '...
-                    'and target dsm in vector form (%d)'], ...
-                        numel(pd), numel(target_dsm_vec));
-    end
-
-    % >@@>
-    % compute correlations between 'pd' and 'sf', store in 'rho'
-    rho=cosmo_corr(pd(:),target_dsm_vec(:), params.type);
-    % <@@<
-
-    % store results
-    ds_sa=struct();
-    ds_sa.samples=rho;
-    ds_sa.sa.labels={'rho'};
-    ds_sa.sa.metric={params.metric};
-    ds_sa.sa.type={params.type};
---
+++ tests/test_target_dsm_corr_measure.m
@@ -0,0 +1,87 @@
+function test_suite=test_target_dsm_corr_measure
+    initTestSuite;
+
+function test_target_dsm_corr_measure_pearson
+    ds=cosmo_synthetic_dataset('ntargets',6,'nchunks',1);
+    mat1=[1 2 3 4 2 3 2 1 2 3 1 2 3 2 2];
+
+    dcm1=cosmo_target_dsm_corr_measure(ds,'target_dsm',mat1);
+    assertElementsAlmostEqual(dcm1.samples,0.2507,'absolute',1e-4);
+    assertEqual(dcm1.sa.labels,{'rho'});
+    assertEqual(dcm1.sa.metric,{'correlation'});
+    assertEqual(dcm1.sa.type,{'Pearson'});
+
+    distance_ds=cosmo_pdist(ds.samples,'correlation');
+    assertElementsAlmostEqual(cosmo_corr(distance_ds',mat1'),dcm1.samples);
+
+    sq1=cosmo_squareform(mat1);
+    dcm2=cosmo_target_dsm_corr_measure(ds,'target_dsm',sq1);
+    assertEqual(dcm1,dcm2);
+
+    dcm3=cosmo_target_dsm_corr_measure(ds,'target_dsm',sq1,...
+                                                'metric','euclidean');
+    assertElementsAlmostEqual(dcm3.samples,0.3037,'absolute',1e-4);
+
+
+function test_target_dsm_corr_measure_partial
+    ds=cosmo_synthetic_dataset('ntargets',6,'nchunks',1);
+    mat1=[1 2 3 4 2 3 2 1 2 3 1 2 3 2 2];
+    mat2=mat1(end:-1:1);
+
+    dcm1=cosmo_target_dsm_corr_measure(ds,'target_dsm',mat1,...
+                                                'regress_dsm',mat2);
+    assertElementsAlmostEqual(dcm1.samples,0.3082,'absolute',1e-4);
+
+
+function test_target_dsm_corr_measure_partial_matlab_correspondence
+    if ~cosmo_check_external('@stats',false)
+        cosmo_notify_test_skipped(['partial correlations '...
+                                    'correspondence not tested; '...'
+                                    'stats toolbox is not available']);
+        return
+    end
+    ds=cosmo_synthetic_dataset('ntargets',6,'nchunks',1);
+    mat1=[1 2 3 4 2 3 2 1 2 3 1 2 3 2 2];
+    mat2=mat1(end:-1:1);
+
+    dcm1=cosmo_target_dsm_corr_measure(ds,'target_dsm',mat1,...
+                                                'regress_dsm',mat2);
+    distance=cosmo_pdist(ds.samples,'correlation');
+    pcorr=partialcorr(distance',mat1',mat2');
+
+    assertElementsAlmostEqual(dcm1.samples,pcorr);
+
+
+function test_target_dsm_corr_measure_non_pearson
+    % test non-Pearson correlations
+    if ~cosmo_check_external('@stats',false)
+        cosmo_notify_test_skipped(['non-Pearson correlations are '...
+                                    'not tested; '...'
+                                    'stats toolbox is not available']);
+        return
+    end
+
+    ds=cosmo_synthetic_dataset('ntargets',6,'nchunks',1);
+    mat1=[1 2 3 4 2 3 2 1 2 3 1 2 3 2 2];
+
+    dcm1=cosmo_target_dsm_corr_measure(ds,'target_dsm',mat1,...
+                                                'type','Spearman');
+
+    assertElementsAlmostEqual(dcm1.samples,0.2558,'absolute',1e-4);
+
+
+
+% test exceptions
+function test_target_dsm_corr_measure_exceptions
+    ds=cosmo_synthetic_dataset('ntargets',6,'nchunks',1);
+    mat1=[1 2 3 4 2 3 2 1 2 3 1 2 3 2 2];
+
+    aet=@(varargin)assertExceptionThrown(...
+                @()cosmo_target_dsm_corr_measure(varargin{:}),'');
+    aet(struct,mat1);
+    aet(ds);
+    aet(ds,'target_dsm',[mat1 1]);
+    aet(ds,'target_dsm',eye(6));
+    aet(ds,'target_dsm',zeros(7));
+
+
