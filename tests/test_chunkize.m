8bb01dfd5cc3fd8ac2e8cc429f446bd144f076f6
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-02-12 20:16:58 +0100

    BF: fix case of many chunkas that need balancing

--- tests/test_chunkize.m
+++ tests/test_chunkize.m
@@ -30,9 +30,7 @@ function test_chunkize_imbalance()
     assertExceptionThrown(@()cosmo_chunkize(ds,2),'');
     ds.sa.chunks=2+[1 1 2 2 2]';
     assertExceptionThrown(@()cosmo_chunkize(ds,2),'');
-    ds.sa.targets=10+[1 1 2 2 2]';
-    assertExceptionThrown(@()cosmo_chunkize(ds,2),'');
-    ds.sa.targets(:)=10+[1 2 1 2 2];
+    ds.sa.targets=10+[1 2 1 2 2]';
     assertExceptionThrown(@()cosmo_chunkize(ds,3),'');
     res=cosmo_chunkize(ds,2);
 
@@ -40,6 +38,24 @@ function test_chunkize_imbalance()
     ds2=cosmo_stack({ds,ds});
     res2=cosmo_chunkize(ds2,2);
     assertEqual(res2,[1 1 2 2 2 1 1 2 2 2]');
+
+function test_all_unique_chunks()
+    ds=struct();
+    ds.samples=(1:5)';
+    ds.sa.targets=2+[1 1 2 2 2]';
+    ds.sa.chunks=10+[1 2 3 4 5]';
+
+    res=cosmo_chunkize(ds,2);
+    assertEqual(res,[1 2 1 2 1]');
+
+    res=cosmo_chunkize(ds,5);
+    assertEqual(res,[1 2 3 4 5]');
+
+
+    assertExceptionThrown(@()cosmo_chunkize(ds,6),'');
+
+
+
 
 
 
