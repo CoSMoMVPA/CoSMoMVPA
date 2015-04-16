a1c263d343140c65af6606664b371aff907bb363
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-04-16 15:05:30 +0200

    TST: unit test for strsplit

---
+++ tests/test_strsplit.m
@@ -0,0 +1,33 @@
+function test_suite = test_strsplit
+    initTestSuite;
+
+function test_suite = test_strsplit_basics
+    aeq=@(a,varargin)assertEqual(cosmo_strsplit(varargin{:}),a);
+
+    aeq({'A','AbbAbA','AbA','A','Ab'},'A*AbbAbA*AbA*A*Ab','*');
+    aeq({'','bbAb','b','*Ab'},'A*AbbAbA*AbA*A*Ab','A*A');
+    aeq('bbAb','A*AbbAbA*AbA*A*Ab','A*A',2);
+    aeq('*Ab','A*AbbAbA*AbA*A*Ab','A*A',-1);
+    aeq({'bb','b'},'A*AbbAbA*AbA*A*Ab','A*A',2,'A');
+    aeq('bb','A*AbbAbA*AbA*A*Ab','A*A',2,'A',1);
+
+    aeq({'a','b','c'},' a b  c ');
+    aeq({'','a','b','','c','','',''},' a b  c   ',' ');
+    aeq({' a b','c',' '},' a b  c   ','  ');
+
+    aeq({'a','b','c'},sprintf('a\nb\t\tc\t\n\t'));
+    aeq({'a','b','','c'},sprintf('a\tb\t\tc'),'\t');
+
+    aeq({'abcd'},'abcd','e');
+
+    aet=@(varargin)assertExceptionThrown(@()...
+                        cosmo_strsplit(varargin{:}),'');
+    aet('A*A','A',1:2);
+
+
+
+
+
+
+
+
