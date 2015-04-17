2407bdb1e78c4c5bb573d1466560bbef244acf99
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-04-17 15:55:42 +0200

    NF+TST: anova_feature_selector

---
+++ tests/test_anova_feature_selector.m
@@ -0,0 +1,23 @@
+function test_suite=test_anova_feature_selector
+    initTestSuite;
+
+function test_anova_feature_selector_basics
+    ds=cosmo_synthetic_dataset();
+
+    aeq=@(arg,res)assertEqual(cosmo_anova_feature_selector(ds,arg),res);
+    aeq(.1,2);
+    aeq(.45,[2 4 5]);
+    aeq(.99,[2 4 5 3 1 6]);
+    aeq(1,2);
+    aeq(3,[2 4 5]);
+    aeq(6,[2 4 5 3 1 6]);
+
+    aet=@(arg)assertExceptionThrown(@()...
+                    cosmo_anova_feature_selector(ds,arg),'');
+
+    aet(1.5);
+    aet(7);
+
+    ds.samples(:,[2 4 5])=NaN;
+    assertEqual(cosmo_anova_feature_selector(ds,3),[3 1 6])
+    assertExceptionThrown(@()cosmo_anova_feature_selector(ds,4),'');
