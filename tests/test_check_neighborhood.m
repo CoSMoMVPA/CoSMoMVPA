4ea2ce0b34ef74c2c7ecb262d00b712fbf47e46c
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-05-17 21:00:11 +0200

    TST: more tests for check_neighborhood

--- tests/test_check_neighborhood.m
+++ tests/test_check_neighborhood.m
@@ -6,13 +6,25 @@ function test_check_neighborhood_exceptions
                 cosmo_check_neighborhood(varargin{:}),'');
     is_ok=@cosmo_check_neighborhood;
 
+    % test four possible scenario's:
+    % 1) two arguments (nh and ds), nh has .origin
+    % 2) two arguments (nh and ds), nh does not have .origin
+    % 3) one arguments nh, nh has .origin
+    % 4) one arguments nh, nh does not have .origin
+
     ds=cosmo_synthetic_dataset();
     nh=[];
     aet(nh);
     nh=struct();
     aet(nh);
+    aet(nh,ds);
+
     nh.fa=struct();
     nh.neighbors='foo';
+
+    aet(nh,ds);
+    aet_wo(nh,ds);
+    aet_wo(nh);
     aet(nh);
 
     nh.neighbors={[1 2];3};
@@ -20,40 +32,60 @@ function test_check_neighborhood_exceptions
     nh.a.vol.dim=[1 2 1];
     nh.origin.a=ds.a;
     nh.origin.fa=ds.fa;
-    is_ok(nh); % should pass
+    is_ok(nh,ds);
+    ok_wo(nh,ds);
+    ok_wo(nh);
+    is_ok(nh);
+
+
 
     nh.neighbors={[1;2];3};
     aet(nh,ds);
+    aet_wo(nh,ds);
+    aet_wo(nh);
     aet(nh);
+
 
     nh.neighbors={[1 2],3};
     aet(nh,ds);
+    aet_wo(nh,ds);
     aet(nh);
 
     nh.neighbors={[1 2];3;4};
     aet(nh,ds);
+    aet_wo(nh,ds);
+    aet_wo(nh);
     aet(nh);
 
     nh.neighbors={[1 2];3};
     nh.foo=struct();
     aet(nh,ds);
+    aet_wo(nh,ds);
+    aet_wo(nh);
     aet(nh);
     nh=rmfield(nh,'foo');
 
     nh.neighbors={[.5 1];3};
     aet(nh,ds);
+    aet_wo(nh,ds);
+    aet_wo(nh);
     aet(nh);
 
     nh.neighbors={[.5 1];struct()};
     aet(nh,ds);
+    aet_wo(nh,ds);
+    aet_wo(nh);
     aet(nh);
 
     nh.neighbors={[1 2];7};
     aet(nh,ds);
+    aet_wo(nh,ds);
+    ok_wo(nh);
     is_ok(nh); % should pass
 
     nh.neighbors={[1 2];3};
     is_ok(nh,ds); % should pass
+    ok_wo(nh,ds);
     is_ok(nh); % should pass
 
     nh.origin.fa=ds.fa;
@@ -62,17 +94,26 @@ function test_check_neighborhood_exceptions
     nh.a=ds.a;
     nh.neighbors=num2cell(1:6)';
     is_ok(nh,ds); % should pass
+    ok_wo(nh,ds);
+    ok_wo(nh);
+    is_ok(nh);
 
     nh.origin.fa=rmfield(nh.fa,'i');
     aet(nh,ds);
+    ok_wo(nh,ds);
+    ok_wo(nh);
     is_ok(nh); % should pass
 
     nh.origin.a.fdim.labels=nh.a.fdim.labels(2:end);
     aet(nh,ds);
+    ok_wo(nh,ds);
+    ok_wo(nh);
     is_ok(nh); % should pass
 
     nh.origin.a.fdim.values=nh.a.fdim.values(2:end);
     aet(nh,ds);
+    ok_wo(nh,ds);
+    ok_wo(nh);
     is_ok(nh); % should pass
     assertFalse(is_ok(nh,false,ds)); % should pass
     assertFalse(is_ok(nh,ds,false)); % should pass
@@ -80,6 +121,8 @@ function test_check_neighborhood_exceptions
     ds.a.fdim.values=ds.a.fdim.values(2:end);
     ds.a.fdim.labels=ds.a.fdim.labels(2:end);
     is_ok(nh,ds); % should pass
+    ok_wo(nh,ds);
+    ok_wo(nh);
     is_ok(nh,true); % should pass
 
 
@@ -87,10 +130,31 @@ function test_check_neighborhood_exceptions
     nh=struct();
     nh.sa=ds.sa;
     nh.neighbors={1;2};
-    aet(nh);
+
     aet(nh,ds);
+    aet_wo(nh,ds);
+    aet_wo(nh);
+    aet(nh);
 
     nh.neighbors={1;2;4;3};
     is_ok(nh,ds); % should pass
+    ok_wo(nh,ds);
+    ok_wo(nh);
     is_ok(nh); % should pass
 
+function nh=remove_origin(nh)
+    if isfield(nh,'origin')
+        nh=rmfield(nh,'origin');
+    end
+
+function aet_wo(nh,varargin)
+    % assert exception thrown without origin
+    nh=remove_origin(nh);
+
+    assertExceptionThrown(@()...
+                cosmo_check_neighborhood(nh,varargin{:}),'');
+
+function ok_wo(nh,varargin)
+    % is ok without origin
+    nh=remove_origin(nh);
+    cosmo_check_neighborhood(nh,varargin{:});
