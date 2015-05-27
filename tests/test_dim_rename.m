0f7fe855c756d4bc191f9bea6fd83ceaed3e3692
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-05-27 15:44:36 +0200

    TST: test for dim_rename

---
+++ tests/test_dim_rename.m
@@ -0,0 +1,26 @@
+function test_suite = test_dim_rename()
+    initTestSuite
+
+function test_dim_rename_basics
+    ds=cosmo_synthetic_dataset();
+
+    % simple rename
+    ds2=cosmo_dim_rename(ds,'j','jj');
+    assertEqual(ds2.a.fdim.labels{2},'jj');
+    assertEqual(ds2.a.fdim.values,ds.a.fdim.values);
+    assertEqual(ds2.samples,ds.samples);
+    assertEqual(ds2.sa,ds.sa);
+
+    fa=ds.fa;
+    fa.jj=fa.j;
+    fa=rmfield(fa,'j');
+    assertEqual(ds2.fa,fa);
+
+    % if raise=false, input is same as output
+    ds3=cosmo_dim_rename(ds,'xxx','jj',false);
+    assertEqual(ds,ds3,ds);
+
+    aet=@(varargin)assertExceptionThrown(@()...
+                        cosmo_dim_rename(varargin{:}),'');
+    aet(ds,'xxx','jj');
+    aet(ds,'xxx','jj',true);
