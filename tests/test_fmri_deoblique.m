4f00caa85a59171b23b2aed378e6283c3815fc69
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-05-29 11:03:43 +0200

    TST: test for fmri deoblique

---
+++ tests/test_fmri_deoblique.m
@@ -0,0 +1,25 @@
+function test_suite = test_fmri_deoblique
+    initTestSuite;
+
+
+function test_fmri_deoblique_basics
+    ds=cosmo_synthetic_dataset('size','normal','ntargets',1,'nchunks',1);
+    % make dataset oblique (manually)
+    ds.a.vol.mat(1,1)=.8;
+    ds.a.vol.mat(2,1)=.6;
+
+    ds_deoblique=cosmo_fmri_deoblique(ds);
+
+    mat=eye(4);
+    mat(2,2)=2;
+    mat(3,3)=2;
+    mat(1:3,4)=[-3.2 -2.4 -3];
+
+    assertEqual(ds_deoblique.a.vol.mat,mat);
+    ds.a.vol.mat=mat;
+    assertEqual(ds,ds_deoblique);
+
+    ds_deoblique2=cosmo_fmri_deoblique(ds_deoblique);
+    assertEqual(ds_deoblique,ds_deoblique2);
+
+
