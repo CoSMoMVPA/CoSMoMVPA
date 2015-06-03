c2de79c5f216b6bea94ac3b3d326d8d95e6d9d47
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-06-03 10:36:45 +0200

    TST+NF: test for type

---
+++ tests/test_type.m
@@ -0,0 +1,34 @@
+function test_suite = test_type()
+    initTestSuite;
+
+function test_type_basics()
+    fn=cosmo_make_temp_filename();
+    cleaner=onCleanup(@()delete(fn));
+
+    data=write_data(fn);
+
+    s=cosmo_type(fn);
+    assertEqual(s,data);
+
+function test_dim_type_fprintf()
+    if cosmo_wtf('is_octave')
+        cosmo_notify_test_skipped('''evalc'' is not available in Octave');
+        return;
+    end
+
+    fn=cosmo_make_temp_filename();
+    cleaner=onCleanup(@()delete(fn));
+
+    data=write_data(fn);
+
+    s=evalc('cosmo_type(fn);');
+    assertEqual(s,data);
+
+
+function data=write_data(fn)
+    fid=fopen(fn,'w');
+    data=sprintf('foo\nbar%s',cosmo_make_temp_filename());
+    fprintf(fid,data);
+    fclose(fid);
+
+
