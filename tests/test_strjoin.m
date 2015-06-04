f55147d8c2d57c1cf2b8cc6d2a54408f6ed88d44
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-06-04 17:40:48 +0200

    TST+NF: test for strjoin

---
+++ tests/test_strjoin.m
@@ -0,0 +1,31 @@
+function test_suite = test_strjoin
+    initTestSuite;
+
+function test_suite = test_strjoin_basics
+    aeq=@(a,varargin)assertEqual(cosmo_strjoin(varargin{:}),a);
+
+    aeq('a b cc',{'a','b','cc'});
+    aeq('a>#<b>#<cc',{'a','b','cc'}, '>#<');
+    aeq(sprintf('a\tb\tcc'),{'a','b','cc'}, '\t');
+    aeq('a\b\cc',{'a','b','cc'}, '\\');
+    aeq('a+b=cc',{'a','b','cc'}, {'+','='});
+
+function test_suite = test_strjoin_exceptions
+    aet=@(varargin)assertExceptionThrown(@()...
+                        cosmo_strjoin(varargin{:}),'');
+
+    % firat argument must be cell with strings
+    aet('a');
+    aet(struct)
+    aet(2);
+    aet([]);
+    aet({2});
+
+    % second argument must be string or cell with strings
+    aet({'a','b','c'},2);
+    aet({'a','b','c'},struct);
+    aet({'a','b','c'},[]);
+    aet({'a','b','c'},[]);
+    aet({'a','b','c'},{'x'});
+    aet({'a','b','c'},{1,2});
+
