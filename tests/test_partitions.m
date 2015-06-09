31d01006a296013e5726194446f328e25611e7e5
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-06-09 15:53:09 +0200

    TST: test for different number of chunks

--- tests/test_partitions.m
+++ tests/test_partitions.m
@@ -60,78 +60,84 @@ function test_nchoosek_partitioner()
     end
 
 function test_nchoosek_partitioner_half()
-    offset=floor(rand()*10+20);
+    offsets=[0 floor(rand()*10+20)];
+    for offset=offsets
+        for nchunks=2:4:10
 
-    for nchunks=2:2:8
+            ds=cosmo_synthetic_dataset('nchunks',nchunks,'ntargets',3);
+            ds.sa.chunks=ds.sa.chunks+offset;
 
-        ds=cosmo_synthetic_dataset('nchunks',nchunks,'ntargets',3);
-        ds.sa.chunks=ds.sa.chunks+offset;
+            p=cosmo_nchoosek_partitioner(ds,'half');
+            combis=nchoosek(1:nchunks,nchunks/2);
 
-        p=cosmo_nchoosek_partitioner(ds,'half');
-        combis=nchoosek(1:nchunks,nchunks/2);
+            n=size(combis,1);
+            assertEqual(numel(p.train_indices),n/2);
+            assertEqual(numel(p.test_indices),n/2);
 
-        n=size(combis,1);
-        assertEqual(numel(p.train_indices),n/2);
-        assertEqual(numel(p.test_indices),n/2);
+            for k=1:n/2
+                tr_idx=find(cosmo_match(ds.sa.chunks-offset,...
+                                                        combis(k,:)));
+                te_idx=find(cosmo_match(ds.sa.chunks-offset,...
+                                                        combis(n-k+1,:)));
 
-        for k=1:n/2
-            tr_idx=find(cosmo_match(ds.sa.chunks-offset,combis(k,:)));
-            te_idx=find(cosmo_match(ds.sa.chunks-offset,combis(n-k+1,:)));
-
-            assertEqual(p.train_indices{k},te_idx);
-            assertEqual(p.test_indices{k},tr_idx);
+                assertEqual(p.train_indices{k},te_idx);
+                assertEqual(p.test_indices{k},tr_idx);
+            end
         end
     end
 
 
 
 function test_nchoosek_partitioner_grouping()
-    nchunks=5;
-    ds=cosmo_synthetic_dataset('nchunks',nchunks,'ntargets',6);
-    ds.sa.modality=mod(ds.sa.targets,2)+1;
-    ds.sa.targets=floor(ds.sa.targets/2);
+    for nchunks=[2 5]
+        ds=cosmo_synthetic_dataset('nchunks',nchunks,'ntargets',6);
+        ds.sa.modality=mod(ds.sa.targets,2)+1;
+        ds.sa.targets=floor(ds.sa.targets/2);
 
-    for n_test=1:3
-        for moda_idx=1:4
+        for n_test=1:(nchunks-1)
+            for moda_idx=1:4
 
-            if moda_idx==3
-                modas={1 2};
-                moda_arg={1 2};
-            elseif moda_idx==4
-                modas={1,2};
-                moda_arg=[];
-            else
-                modas={moda_idx};
-                moda_arg=moda_idx;
-            end
-
-            n_moda=numel(modas);
-
-            p=cosmo_nchoosek_partitioner(ds,n_test,'modality',moda_arg);
-            combis=nchoosek(1:nchunks,n_test);
-            n_combi=size(combis,1);
-
-            n_folds=numel(p.train_indices);
-            assertEqual(numel(p.test_indices),n_folds);
-            assertEqual(n_folds,n_combi*n_moda);
-
-            visited_count=zeros(1,n_folds);
-            for m=1:n_moda
-                for j=1:n_combi
-                    tr_msk=~cosmo_match(ds.sa.chunks,combis(j,:)) & ...
-                                ~cosmo_match(ds.sa.modality,modas{m});
-                    te_msk=cosmo_match(ds.sa.chunks,combis(j,:)) & ...
-                                cosmo_match(ds.sa.modality,modas{m});
-                    tr_idx=find_fold(p.train_indices,tr_msk);
-                    te_idx=find_fold(p.test_indices,te_msk);
-                    assertEqual(tr_idx,te_idx);
-                    visited_count(tr_idx)=visited_count(tr_idx)+1;
+                if moda_idx==3
+                    modas={1 2};
+                    moda_arg={1 2};
+                elseif moda_idx==4
+                    modas={1,2};
+                    moda_arg=[];
+                else
+                    modas={moda_idx};
+                    moda_arg=moda_idx;
                 end
+
+                n_moda=numel(modas);
+
+                p=cosmo_nchoosek_partitioner(ds,n_test,'modality',...
+                                                            moda_arg);
+                combis=nchoosek(1:nchunks,n_test);
+                n_combi=size(combis,1);
+
+                n_folds=numel(p.train_indices);
+                assertEqual(numel(p.test_indices),n_folds);
+                assertEqual(n_folds,n_combi*n_moda);
+
+                % each fold must be present exactly once
+                visited_count=zeros(1,n_folds);
+                for m=1:n_moda
+                    for j=1:n_combi
+                        tr_msk=~cosmo_match(ds.sa.chunks,combis(j,:)) & ...
+                                    ~cosmo_match(ds.sa.modality,modas{m});
+                        te_msk=cosmo_match(ds.sa.chunks,combis(j,:)) & ...
+                                    cosmo_match(ds.sa.modality,modas{m});
+                        tr_idx=find_fold(p.train_indices,tr_msk);
+                        te_idx=find_fold(p.test_indices,te_msk);
+                        assertEqual(tr_idx,te_idx);
+                        visited_count(tr_idx)=visited_count(tr_idx)+1;
+                    end
+                end
+
+                assertEqual(visited_count,ones(1,n_folds));
+
+
             end
-
-            assertEqual(visited_count,ones(1,n_folds));
-
-
         end
     end
 
