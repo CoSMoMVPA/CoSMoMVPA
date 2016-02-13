2eeacdab8e0dbfefe3c807adce1875c6a9e97641
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2016-02-13 12:09:49 +0100

    EXC: move exercise code

--- examples/run_operations_on_datasets.m
+++ examples/run_operations_on_datasets.m
@@ -20,16 +20,64 @@ ds.sa.targets = repmat((1:6)',10,1);
 % set ds.sa.chunks (acquistion run number) to the 60x1 column vector:
 % [ 1 1 1 1 1 1 2 2 2 ... 10 10 ]'
 % >@@>
-chunks = repmat(1:10, [6, 1]);
-chunks = chunks(:);
-ds.sa.chunks = chunks;
+ds.sa.chunks = ceil((1:60)'/6);
 % <@@<
+
+% set the labels
+labels = repmat({   'monkey';
+                    'lemur';
+                    'mallard';
+                    'warbler';
+                    'ladybug';
+                    'lunamoth'},10,1);
+ds.sa.labels = labels;
 
 % This particular ROI has a few constant features due to
 % not-so-agressive masking, so remove constant features
 ds=cosmo_remove_useless_data(ds);
 
-%% Set the sample indices that correspond to primates and bugs
+
+
+
+%% Various slicing operations on the samples
+
+% only get data in chunks 1 and 2.
+% Make a logical mask indicating where .ds.chunks less than or equal to 2,
+% then use cosmo_slice to select these samples
+% (hint: you can use cosmo_match or just '<=')
+% >@@>
+chunks12_msk=ds.sa.chunks<=2;
+ds_chunks12=cosmo_slice(ds,chunks12_msk);
+cosmo_disp(ds_chunks12);
+% <@@<
+
+% only get data in conditions 1 and 3
+% >@@>
+% (there are multiple ways of doing this)
+targets_13=ds.sa.targets==1 | ds.sa.targets==3; % element-wise logical 'or'
+ds_targets13=cosmo_slice(ds,targets_13);
+cosmo_disp(ds_targets13);
+
+% this is an alternative way to achieve the same goal
+targets_13_alt=cosmo_match(ds.sa.targets,[1 3]); % using cosmo_match
+ds_targets13_alt=cosmo_slice(ds,targets_13_alt);
+cosmo_disp(ds_targets13_alt);
+
+% sanity check showing they are the same
+assert(isequal(ds_targets13,ds_targets13_alt));
+
+% alterative using cosmo_match and the labels
+labels_monkey_or_mallard=cosmo_match(ds.sa.labels,{'monkey','mallard'});
+ds_targets13_alt2=cosmo_slice(ds,labels_monkey_or_mallard);
+cosmo_disp(ds_targets13_alt2);
+
+% sanity check showing they are the same
+assert(isequal(ds_targets13,ds_targets13_alt2));
+% <@@<
+
+
+% Get the sample indices that correspond to primates and bugs,
+% and assign the result to varibales 'primate_idx' and 'bug_idx'
 
 % >@@>
 primate_idx = ds.sa.targets <= 2;
