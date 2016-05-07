a0ad2a82df12c345afc02c67827045a31d641ebe
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2016-05-07 14:50:21 +0200

    TST: basic unit test for exceptions in plot_slices

---
+++ tests/test_plot_slices.m
@@ -0,0 +1,32 @@
+function test_suite = test_plot_slices
+% tests for plot_slices
+%
+% only includes testing for exceptions, as we don't use GUI in testing
+%
+% #   For CoSMoMVPA's copyright information and license terms,   #
+% #   see the COPYING file distributed with CoSMoMVPA.           #
+
+    initTestSuite;
+
+
+function test_plot_slices_exceptions()
+    aet=@(varargin)assertExceptionThrown(@()...
+                        cosmo_plot_slices(varargin{:}),'');
+    % empty struct
+    aet(struct);
+
+    % dataset with more than one volume
+    ds=cosmo_synthetic_dataset();
+    assert(size(ds.samples,1)>1);
+    aet(ds);
+
+    % 4D array
+    aet(rand([3,3,3,3]))
+
+    % 5D array
+    aet(rand([2,2,2,2]))
+
+    % MEG dataset
+    ds=cosmo_synthetic_dataset('type','meeg','ntargets',1,'nchunks',1);
+    assert(size(ds.samples,1)==1);
+    aet(ds);
