ad46d0cc8de08d6cb27c35cce6fd46a836165572
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2016-11-27 18:03:42 +0100

    TST: show progress instead of skipping test when on CI platform

--- tests/test_montecarlo_cluster_stat_distribution.m
+++ tests/test_montecarlo_cluster_stat_distribution.m
@@ -15,17 +15,14 @@ function test_mccs_uniformity_slow()
     % show progress when running travis - otherwise the test may stall
     % when no output is received for a long time
     is_running_ci=~isempty(getenv('CI'));
+    show_progress=false;
     if is_running_ci
         stack=dbstack();
         names={stack.name};
 
         is_running_MoCov_coverage=any(cosmo_match({'mocov'},names));
         if is_running_MoCov_coverage
-            reason=['Running coverage on CI platform for a very slow '...
-                        'function, which may lead to a stalled build. '];
-
-            cosmo_notify_test_skipped(reason);
-            return;
+            show_progress=true;
         end
     end
 
@@ -62,7 +59,7 @@ function test_mccs_uniformity_slow()
 
     niter=10;
     for attempt=1:max_attempts
-        ps_cell{attempt}=helper_mccs_get_pvalues(niter);
+        ps_cell{attempt}=helper_mccs_get_pvalues(niter,show_progress);
         ps=sort(cat(1,ps_cell{:}));
 
         % compute correlation with expected p-values
@@ -85,10 +82,12 @@ function test_mccs_uniformity_slow()
         end
 
         if count_pass>=min_pass_or_fail_count
+            finalize_test_helepr(show_progress);
             return;
 
         elseif count_fail>=min_pass_or_fail_count
             % enough evidence that p values are non-uniform, fail
+            finalize_test_helepr(show_progress);
             error(['Found z=%d, indicating that probability values '...
                         'are probably not uniform'],z);
         end
@@ -98,11 +97,15 @@ function test_mccs_uniformity_slow()
         niter=ceil(niter*grow_niter);
 
     end
-
+    finalize_test_helepr(show_progress);
     error('Maximum number of attempts reached');
 
+function finalize_test_helepr(show_progress)
+    if show_progress
+        fprintf('\n');
+    end
 
-function ps=helper_mccs_get_pvalues(niter)
+function ps=helper_mccs_get_pvalues(niter,show_progress)
     % output: correlation between expected uniform distribution of p values
     % and those obtained from monte_carl_cluster_stat
 
@@ -125,6 +128,8 @@ function ps=helper_mccs_get_pvalues(niter)
     nh.fa=struct();
     nh.fa.sizes=1;
     nh.a=struct();
+    nh.origin.fa=ds.fa;
+    nh.origin.a=ds.a;
 
     for iter=1:niter
         opt=struct();
@@ -137,6 +142,10 @@ function ps=helper_mccs_get_pvalues(niter)
 
         z=cosmo_montecarlo_cluster_stat(ds,nh,opt);
         ps(iter)=normcdf(z.samples);
+
+        if show_progress
+            fprintf(':');
+        end
     end
 
 
