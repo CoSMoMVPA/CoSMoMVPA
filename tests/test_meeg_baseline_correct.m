7736b59d0959d27b345a825b9a716e66c3f5cae2
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2016-12-02 16:26:20 +0100

    TST: more tests for random data for MEEG baseline correction

--- tests/test_meeg_baseline_correct.m
+++ tests/test_meeg_baseline_correct.m
@@ -6,53 +6,142 @@ function test_suite = test_meeg_baseline_correct
 
     initTestSuite;
 
-function test_meeg_baseline_correct_ft_comparison()
-    if cosmo_skip_test_if_no_external('fieldtrip')
-        return;
-    end
-
-    if cosmo_wtf('is_octave')
-        cosmo_notify_test_skipped(['ft_freqbaseline is not compatible '...
-                                        'with Octave']);
-    end
-
-    interval=[-.15 -.04];
-    [ds,ds_ref]=get_test_dataset(interval);
-
+function test_meeg_baseline_correct_ft_comparison_methods()
     methods={'relative','absolute','relchange'};
+    references={'manual','ft','ds'};
 
-    ft=cosmo_map2meeg(ds);
+    combis=cosmo_cartprod({methods,references});
+    ncombis=size(combis,1);
 
-    for k=1:numel(methods)
-        method=methods{k};
-        opt=struct();
-        opt.baseline=interval;
-        opt.baselinetype=method;
+    for k=1:ncombis
+        combi=combis(k,:);
+        method=combi{1};
+        reference=combi{2};
 
+        % random interval
+        start=rand(1)*-.25;
+        dur=.05+rand(1)*.2;
+        interval=start+[0 dur];
 
-        % (unsupported in octave)
-        ft_bl=ft_freqbaseline(opt,ft);
-
-        ds_ft_bl=cosmo_meeg_dataset(ft_bl);
-
-        if cosmo_isfield(ds_ft_bl','a.hdr_ft.cfg')
-            ds_ft_bl.a.hdr_ft=rmfield(ds_ft_bl.a.hdr_ft,'cfg');
-        end
-
-        ds_ft_msk=ds_ft_bl.fa.chan==3 & ds_ft_bl.fa.freq==2;
-        d_ft=cosmo_slice(ds_ft_bl,ds_ft_msk,2);
-        if isfield(d_ft.a.meeg,'senstype')
-            d_ft.a.meeg=rmfield(d_ft.a.meeg,'senstype');
-        end
-
-        ds_bl=cosmo_meeg_baseline_correct(ds,ds_ref,method);
-        ds_bl_msk=ds_bl.fa.chan==3 & ds_bl.fa.freq==2;
-        d=cosmo_slice(ds_bl,ds_bl_msk,2);
-
-        assertElementsAlmostEqual(d_ft.samples,d.samples);
-        assertEqual(d_ft.fa,d.fa);
-        assertEqual(d_ft.a.fdim,d.a.fdim);
+        helper_test_meeg_baseline_correct_comparison(interval,...
+                                                        method,reference);
     end
+
+
+function helper_test_meeg_baseline_correct_comparison(interval,method,...
+                                                    reference)
+
+    switch reference
+        case 'ft'
+            if cosmo_skip_test_if_no_external('fieldtrip')
+                return;
+            end
+
+            if cosmo_wtf('is_octave')
+                cosmo_notify_test_skipped(['ft_freqbaseline is not '...
+                                                'compatible with Octave']);
+            end
+
+        case 'ds'
+
+        case 'manual'
+
+        otherwise
+            assert(false,'this should not happen');
+    end
+
+    ds=cosmo_synthetic_dataset('type','timefreq','size','big','seed',0);
+    ds.samples=randn(size(ds.samples));
+
+    m=ds.fa.chan<=3 & ds.fa.freq<=2;
+    ds=cosmo_slice(ds,m,2);
+    ds=cosmo_dim_prune(ds);
+
+    y=cosmo_meeg_baseline_correct(ds,interval,method);
+
+    % (unsupported in octave)
+    switch reference
+        case 'ft'
+            ft=cosmo_map2meeg(ds);
+
+            opt=struct();
+            opt.baseline=interval;
+            opt.baselinetype=method;
+
+            ft_bl=ft_freqbaseline(opt,ft);
+            x=cosmo_meeg_dataset(ft_bl);
+
+        case 'ds'
+            msk=cosmo_dim_match(ds,'time',...
+                        @(t) t>=min(interval) & t<=max(interval));
+            ds_ref=cosmo_slice(ds,msk,2);
+            x=cosmo_meeg_baseline_correct(ds,ds_ref,method);
+
+        case 'manual'
+            msk=cosmo_dim_match(ds,'time',...
+                        @(t) t>=min(interval) & t<=max(interval));
+            ds_ref=cosmo_slice(ds,msk,2);
+
+            x=ds;
+
+            for chan=1:max(ds.fa.chan)
+                for freq=1:max(ds.fa.freq)
+                    msk=ds.fa.chan==chan & ds.fa.freq==freq;
+                    s=ds.samples(:,msk);
+
+                    ref_msk=ds_ref.fa.chan==chan & ds_ref.fa.freq==freq;
+                    r=mean(ds_ref.samples(:,ref_msk),2);
+
+                    switch method
+                        case 'absolute'
+                            v=bsxfun(@minus,s,r);
+
+                        case 'relative'
+                            v=bsxfun(@rdivide,s,r);
+
+                        case 'relchange'
+                            v=bsxfun(@rdivide,bsxfun(@minus,s,r),r);
+
+
+                        otherwise
+                            error('not supported: %s', method);
+                    end
+
+                   x.samples(:,msk)=v;
+                end
+            end
+
+
+        otherwise
+            assert(false)
+
+    end
+
+    x_unq=cosmo_index_unique({x.fa.time,x.fa.chan});
+    y_unq=cosmo_index_unique({y.fa.time,y.fa.chan});
+
+    n=numel(x_unq);
+    max_n_to_choose=10;
+    n_to_choose=min(max_n_to_choose,n);
+    rp=cosmo_randperm(n,n_to_choose);
+    assert(numel(x_unq)==numel(y_unq));
+    for j=1:n_to_choose;
+        idx=rp(j);
+        x_sel=cosmo_slice(x,x_unq{idx},2,false);
+        y_sel=cosmo_slice(y,y_unq{idx},2,false);
+
+        p=x_sel.samples;
+        q=y_sel.samples;
+
+        desc=sprintf('method %s, reference %s',method,reference);
+        assertElementsAlmostEqual(p,q,...
+                        'relative',1e-6,desc);
+        assertEqual(x_sel.fa,y_sel.fa);
+
+    end
+
+    assertEqual(x.a.fdim,y.a.fdim);
+
 
 function test_meeg_baseline_correct_regression()
     interval=[-.15 -.04];
