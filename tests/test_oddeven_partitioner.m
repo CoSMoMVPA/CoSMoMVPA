a69f1ab0c5247f085418323973a5160e2c77f4d0
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2017-01-19 20:33:04 +0100

    TST+BIG: change test functions to be able to run test at recent (2016 and later) versions of Matlab

--- tests/test_oddeven_partitioner.m
+++ tests/test_oddeven_partitioner.m
@@ -3,7 +3,10 @@ function test_suite=test_oddeven_partitioner
 %
 % #   For CoSMoMVPA's copyright information and license terms,   #
 % #   see the COPYING file distributed with CoSMoMVPA.           #
-
+    try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+        test_functions=localfunctions();
+    catch % no problem; early Matlab versions can use initTestSuite fine
+    end
     initTestSuite;
 
 function test_test_oddeven_partitioner_basics
